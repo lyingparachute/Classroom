59c2b332e980433106336bfc7a86a1f5fa898c3b
lyingparachute <igor1501@o2.pl>
2022-11-23 19:41:27 +0100

    Add example fields of study

--- src/main/java/systems/ultimate/classroom/enums/FieldOfStudy.java
+++ src/main/java/systems/ultimate/classroom/enums/FieldOfStudy.java
@@ -1,5 +1,19 @@
 package systems.ultimate.classroom.enums;
 
 public enum FieldOfStudy {
+    ROBOTICS("Automatic Control, Cybernetics and Robotics"),
+    ELECTRONICS("Electronics and Telecommunications"),
+    ELECTRICAL("Electrical Engineering"),
+    INFORMATICS("Distributed Applications and Internet Services");
+
+    private final String desc;
+    FieldOfStudy (String desc){
+        this.desc = desc;
+    }
+
+    public String description() {
+        return desc;
+    }
+
 
 }
