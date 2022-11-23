b6f41adb6ec517370db1726dd92abfc48015985c
lyingparachute <igor1501@o2.pl>
2022-11-23 20:28:28 +0100

    add students endpoint

--- src/main/java/systems/ultimate/classroom/controller/StudentController.java
+++ src/main/java/systems/ultimate/classroom/controller/StudentController.java
@@ -1,4 +1,25 @@
 package systems.ultimate.classroom.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import systems.ultimate.classroom.service.StudentService;
+
+@Controller
+@RequestMapping("students")
 public class StudentController {
+
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String getStudents(Model model) {
+        model.addAttribute("students", service.getAll());
+        return "students";
+    }
+
 }
