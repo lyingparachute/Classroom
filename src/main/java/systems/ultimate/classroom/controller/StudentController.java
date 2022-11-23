4febbaaa72d896b06a15801cda528371ae8b40d1
lyingparachute <igor1501@o2.pl>
2022-11-23 21:15:57 +0100

    added get by id mapping

--- src/main/java/systems/ultimate/classroom/controller/StudentController.java
+++ src/main/java/systems/ultimate/classroom/controller/StudentController.java
@@ -3,7 +3,9 @@ package systems.ultimate.classroom.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import systems.ultimate.classroom.dto.StudentDto;
 import systems.ultimate.classroom.service.StudentService;
 
 @Controller
@@ -18,8 +20,14 @@ public class StudentController {
 
     @GetMapping
     public String getStudents(Model model) {
-        model.addAttribute("students", service.getAll());
+        model.addAttribute("students", service.fetchAll());
         return "students";
     }
 
+    @GetMapping("{id}")
+    public String getStudent(@PathVariable Long id, Model model) {
+        StudentDto studentDto = service.fetchById(id);
+        model.addAttribute("student", studentDto);
+        return "student";
+    }
 }
