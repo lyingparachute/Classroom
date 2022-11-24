6518e4c9e66f28c2cfbf51e5fd762e815d640538
lyingparachute <igor1501@o2.pl>
2022-11-24 12:07:46 +0100

    add class

--- src/main/java/systems/ultimate/classroom/controller/TeacherController.java
+++ src/main/java/systems/ultimate/classroom/controller/TeacherController.java
@@ -1,4 +1,8 @@
 package systems.ultimate.classroom.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TeacherController {
+
 }
--- src/main/java/systems/ultimate/classroom/dto/StudentDto.java
+++ src/main/java/systems/ultimate/classroom/dto/StudentDto.java
@@ -1,6 +1,8 @@
 package systems.ultimate.classroom.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import systems.ultimate.classroom.entity.Teacher;
 import systems.ultimate.classroom.enums.FieldOfStudy;
 
@@ -18,5 +20,4 @@ public class StudentDto implements Serializable {
     private String email;
     private FieldOfStudy fieldOfStudy;
     private List<Teacher> teachersList;
-
 }
