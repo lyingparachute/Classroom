b47dbda9ac9ab85955519485e72a31c31043096f
lyingparachute <igor1501@o2.pl>
2022-11-24 18:50:43 +0100

    add custom method

--- src/main/java/systems/ultimate/classroom/repository/TeacherRepository.java
+++ src/main/java/systems/ultimate/classroom/repository/TeacherRepository.java
@@ -1,8 +1,13 @@
 package systems.ultimate.classroom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import systems.ultimate.classroom.entity.Teacher;
+
+import java.util.List;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
+    @Query("select t from Teacher t where t.firstName like %?1% or t.lastName like %?1%")
+    List<Teacher> findAllByFirstNameOrLastName(String name);
 }
