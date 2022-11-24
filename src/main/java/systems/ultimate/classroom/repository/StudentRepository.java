c14fed1cd0f452f49cbc4cc5d2f5086d68545400
lyingparachute <igor1501@o2.pl>
2022-11-24 10:38:39 +0100

    Add method findAllByFirstNameOrLastName

--- src/main/java/systems/ultimate/classroom/repository/StudentRepository.java
+++ src/main/java/systems/ultimate/classroom/repository/StudentRepository.java
@@ -1,7 +1,13 @@
 package systems.ultimate.classroom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import systems.ultimate.classroom.entity.Student;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("select s from Student s where s.firstName like %?1% or s.lastName like %?1%")
+    List<Student> findAllByFirstNameOrLastName(String firstName);
 }
