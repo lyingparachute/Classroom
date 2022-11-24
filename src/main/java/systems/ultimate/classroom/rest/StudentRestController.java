df368b06044de3855955b16c6af5b98bcc757a12
lyingparachute <igor1501@o2.pl>
2022-11-24 09:49:45 +0100

    Add endpoints in rest controller

--- src/main/java/systems/ultimate/classroom/rest/StudentRestController.java
+++ src/main/java/systems/ultimate/classroom/rest/StudentRestController.java
@@ -1,10 +1,8 @@
 package systems.ultimate.classroom.rest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import systems.ultimate.classroom.dto.StudentDto;
 import systems.ultimate.classroom.service.StudentService;
 
@@ -26,5 +24,35 @@ public class StudentRestController {
         return students.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(students);
     }
 
+    @PostMapping("create")
+    public ResponseEntity<StudentDto> createStudent(@RequestBody StudentDto studentDto) {
+        StudentDto created = studentService.create(studentDto);
+        return created != null ?
+                ResponseEntity.status(HttpStatus.CREATED)
+                        .body(created) :
+                ResponseEntity.badRequest().build();
+    }
 
+    @GetMapping("{id}")
+    public ResponseEntity<StudentDto> getStudent(@PathVariable Long id) {
+        StudentDto studentDto = studentService.fetchById(id);
+        return studentDto != null ?
+                ResponseEntity.ok(studentDto) :
+                ResponseEntity.notFound().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<StudentDto> putStudent(@RequestBody StudentDto studentDto){
+        StudentDto updated = studentService.update(studentDto);
+        return updated != null ?
+                ResponseEntity.status(HttpStatus.CREATED)
+                        .body(updated) :
+                ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
+        studentService.remove(id);
+        return ResponseEntity.accepted().build();
+    }
 }
