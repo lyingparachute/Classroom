d1a2564afeb3f5b55a2c5896af673b033aa1325b
lyingparachute <igor1501@o2.pl>
2022-11-25 17:04:35 +0100

    Add validation for teacher

--- src/main/java/systems/ultimate/classroom/entity/Teacher.java
+++ src/main/java/systems/ultimate/classroom/entity/Teacher.java
@@ -3,9 +3,14 @@ package systems.ultimate.classroom.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import systems.ultimate.classroom.enums.Subject;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,12 +24,24 @@ public class Teacher {
     @GeneratedValue
     private Long id;
 
+    @NotNull
+    @NotEmpty(message = "{message.empty}")
+    @Length(min = 2, max = 60, message = "{message.name}")
     private String firstName;
+
+    @NotNull
+    @NotEmpty(message = "{message.empty}")
+    @Length(min = 2, max = 60, message = "{message.last.name}")
 
     private String lastName;
 
+    @NotNull
+    @Min(value = 18, message = "{message.min.age}")
     private int age;
 
+    @NotNull
+    @NotEmpty(message = "{message.empty}")
+    @Email(message = "{message.valid.email}")
     private String email;
 
     @Enumerated(EnumType.STRING)
