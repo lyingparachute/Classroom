6a2b54294e7d59e7bcb6ae8f209f0c9101ca4745
lyingparachute <igor1501@o2.pl>
2022-11-25 08:12:17 +0100

    add home page

---
+++ src/main/java/systems/ultimate/classroom/controller/HomeController.java
@@ -0,0 +1,12 @@
+package systems.ultimate.classroom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping
+    public String getHome(){
+        return "home";
+    }
+}
