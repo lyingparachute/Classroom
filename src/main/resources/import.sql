268fb28edfcf84591d24b674996823966be784a3
lyingparachute <igor1501@o2.pl>
2022-12-01 14:27:57 +0100

    Add import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,17 @@
+INSERT INTO classroom.student(id, first_name, last_name, email, age, `field_of_study`)
+                   VALUES (1, 'Jan', 'Wawrzyniak', 'j.wawrzyniak@gmail.com', 22, 'ROBOTICS');
+INSERT INTO classroom.student(id, first_name, last_name, email, age, `field_of_study`)
+                   VALUES (2, 'Maciej', 'Komaranczuk', 'm.komaranczuk@gmail.com', 25, 'INFORMATICS');
+INSERT INTO classroom.student(id, first_name, last_name, email, age, `field_of_study`)
+                   VALUES (3, 'Weronika', 'Romanski', 'w.romanski@gmail.com', 20, 'ELECTRICAL');
+INSERT INTO classroom.student(id, first_name, last_name, email, age, `field_of_study`)
+                   VALUES (4, 'Robert', 'Kaczuk', 'r.kaczuk@gmail.com', 21, 'ELECTRONICS');
+
+INSERT INTO classroom.teacher(id, first_name, last_name, email, age, `subject`)
+                   VALUES (1, 'Grzegorz', 'Kostro', 'g.kostro@gmail.com', 48, 'MATHS');
+INSERT INTO classroom.teacher(id, first_name, last_name, email, age, `subject`)
+                   VALUES (2, 'Jagoda', 'Kowalska', 'j.kowalska@gmail.com', 33, 'SCIENCE');
+INSERT INTO classroom.teacher(id, first_name, last_name, email, age, `subject`)
+                   VALUES (3, 'Adam', 'Batista', 'a.batista@gmail.com', 40, 'ART');
+INSERT INTO classroom.teacher(id, first_name, last_name, email, age, `subject`)
+                   VALUES (4, 'Jarosław', 'Adamczuk', 'j.adamski@gmail.com', 55, 'IT');
