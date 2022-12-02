f2b793ea10533b643552f836c8ab8fd697876a16
lyingparachute <igor1501@o2.pl>
2022-12-02 19:29:37 +0100

    Add test for method get

--- src/test/java/systems/ultimate/classroom/rest/TeacherRestControllerTest.java
+++ src/test/java/systems/ultimate/classroom/rest/TeacherRestControllerTest.java
@@ -6,8 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import systems.ultimate.classroom.dto.TeacherDto;
+import systems.ultimate.classroom.entity.Teacher;
 import systems.ultimate.classroom.repository.TeacherRepository;
 import systems.ultimate.classroom.repository.util.InitData;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TeacherRestControllerTest {
@@ -29,8 +39,23 @@ class TeacherRestControllerTest {
     }
 
     @Test
-    void shouldGetAllTeachers() {
-
+    void shouldGetTeacher() throws URISyntaxException {
+        //given
+        Teacher teacher = initData.createFirstTeacher();
+        //when
+        URI url = createURL("/api/teachers/" + teacher.getId());
+        ResponseEntity<TeacherDto> response = restTemplate.getForEntity(url, TeacherDto.class);
+        //then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        TeacherDto actual = response.getBody();
+        assertThat(actual).isNotNull();
+        assertThat(actual.getId()).isEqualTo(teacher.getId());
+        assertThat(actual.getFirstName()).isEqualTo(teacher.getFirstName());
+        assertThat(actual.getLastName()).isEqualTo(teacher.getLastName());
+        assertThat(actual.getEmail()).isEqualTo(teacher.getEmail());
+        assertThat(actual.getAge()).isEqualTo(teacher.getAge());
+        assertThat(actual.getSubject()).isEqualTo(teacher.getSubject());
+        assertThat(actual.getStudentsList()).isEmpty();
     }
 
     @Test
@@ -48,4 +73,9 @@ class TeacherRestControllerTest {
     @Test
     void shouldDeleteTeacher() {
     }
+
+    private URI createURL(String path) throws URISyntaxException {
+        return new URI("http://localhost:" + randomServerPort + path);
+    }
+
 }
