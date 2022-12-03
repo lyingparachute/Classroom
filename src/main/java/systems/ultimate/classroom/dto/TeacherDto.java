a1ba0bb25877c42092d849bfa6e188467c1a6d4d
lyingparachute <igor1501@o2.pl>
2022-12-03 12:56:51 +0100

    Change to dto

--- src/main/java/systems/ultimate/classroom/dto/TeacherDto.java
+++ src/main/java/systems/ultimate/classroom/dto/TeacherDto.java
@@ -3,7 +3,6 @@ package systems.ultimate.classroom.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import systems.ultimate.classroom.entity.Student;
 import systems.ultimate.classroom.enums.Subject;
 
 import java.io.Serializable;
@@ -19,7 +18,7 @@ public class TeacherDto implements Serializable {
     private int age;
     private String email;
     private Subject subject;
-    private Set<Student> studentsList;
+    private Set<StudentDto> studentsList;
 
     @Override
     public String toString() {
