d5523b3d54e47fa26b2d84a17d3799398abab6bb
lyingparachute <igor1501@o2.pl>
2022-12-05 19:26:06 +0100

    Add json ignore

--- src/main/java/systems/ultimate/classroom/entity/Student.java
+++ src/main/java/systems/ultimate/classroom/entity/Student.java
@@ -1,5 +1,6 @@
 package systems.ultimate.classroom.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -46,6 +47,7 @@ public class Student {
     @Enumerated(EnumType.STRING)
     private FieldOfStudy fieldOfStudy;
 
+    @JsonIgnore
     @ManyToMany(fetch = FetchType.LAZY,
             cascade = {
                     CascadeType.MERGE,
