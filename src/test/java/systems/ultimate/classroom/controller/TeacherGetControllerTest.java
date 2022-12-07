d7f00ce25224a183451e29378940ab9b6937bbc5
lyingparachute <igor1501@o2.pl>
2022-12-07 21:12:23 +0100

    create test for get methods

---
+++ src/test/java/systems/ultimate/classroom/controller/TeacherGetControllerTest.java
@@ -0,0 +1,22 @@
+package systems.ultimate.classroom.controller;
+
+import org.junit.jupiter.api.Test;
+
+class TeacherGetControllerTest {
+
+    @Test
+    void getTeachers() {
+    }
+
+    @Test
+    void getPaginatedTeachers() {
+    }
+
+    @Test
+    void searchTeachers() {
+    }
+
+    @Test
+    void getTeacher() {
+    }
+}
