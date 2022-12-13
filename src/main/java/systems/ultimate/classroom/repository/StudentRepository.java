c3360e7279e33f6ec33b924d53eaa976bd004c28
lyingparachute <igor1501@o2.pl>
2022-12-13 21:57:51 +0100

    Add new method

--- src/main/java/systems/ultimate/classroom/repository/StudentRepository.java
+++ src/main/java/systems/ultimate/classroom/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package systems.ultimate.classroom.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import systems.ultimate.classroom.entity.Student;
@@ -9,5 +11,8 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     @Query("select s from Student s where s.firstName like %?1% or s.lastName like %?1%")
-    List<Student> findAllByFirstNameOrLastName(String firstName);
+    List<Student> findAllByFirstNameOrLastName(String name);
+
+    @Query("select s from Student s where s.firstName like %?1% or s.lastName like %?1%")
+    Page<Student> findAllByFirstNameOrLastName(String name, Pageable pageable);
 }
