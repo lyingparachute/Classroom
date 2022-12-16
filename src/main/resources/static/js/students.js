28f7043f5640819c05c099d8f1952b1f5a36d25b
lyingparachute <igor1501@o2.pl>
2022-12-16 20:55:11 +0100

    Fix modal names and move scripts to external file

---
+++ src/main/resources/static/js/students.js
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+$(document).ready(function () {
+    $("#btnClear").on("click", function (e) {
+        e.preventDefault();
+        $("#name").text("");
+        window.location = "/students";
+    });
+
+    $('.delBtn').on('click', function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        $('#exampleModal #delRef').attr('href', href);
+    });
+});
+
+function changePageSize() {
+    $("#studentForm").submit();
+}
