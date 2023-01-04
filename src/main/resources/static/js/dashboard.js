742576dab5dcf0cd500fe79b16fed8faca45410f
lyingparachute <igor1501@o2.pl>
2023-01-04 15:04:25 +0100

    Add custom styling to navbar on scroll

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -39,3 +39,41 @@ var myChart = new Chart(ctx, {
         },
     },
 });
+
+(function () {
+    "use strict";
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+     * Easy on scroll event listener
+     */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+    /**
+     * Toggle .header-scrolled class to #header when page is scrolled
+     */
+    let selectHeader = select('#main-navbar')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('navbar-scrolled')
+            } else {
+                selectHeader.classList.remove('navbar-scrolled')
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+})()
