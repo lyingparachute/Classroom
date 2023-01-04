c04074bae51920953a6969e15e6aa8a8c129840d
lyingparachute <igor1501@o2.pl>
2023-01-04 16:13:56 +0100

    Add all views of dashboard

--- src/main/java/systems/ultimate/classroom/controller/DashboardController.java
+++ src/main/java/systems/ultimate/classroom/controller/DashboardController.java
@@ -13,9 +13,39 @@ public class DashboardController {
         return "dashboard/dashboard";
     }
 
+    @GetMapping("/subjects")
+    public String getSubjects(){
+        return "dashboard/subjects";
+    }
+
+    @GetMapping("/fieldsOfStudy")
+    public String getFieldsOfStudy(){
+        return "dashboard/fieldsOfStudy";
+    }
+
+    @GetMapping("/library")
+    public String getLibrary(){
+        return "dashboard/library";
+    }
+
+    @GetMapping("/facilities")
+    public String getFacilities(){
+        return "dashboard/facilities";
+    }
+
     @GetMapping("/calendar")
     public String getCalendar(){
         return "dashboard/calendar";
+    }
+
+    @GetMapping("/analytics")
+    public String getAnalytics(){
+        return "dashboard/analytics";
+    }
+
+    @GetMapping("/sales")
+    public String getSales(){
+        return "dashboard/sales";
     }
 
     @GetMapping("/settings")
