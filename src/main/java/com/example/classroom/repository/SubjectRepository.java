7bf8f981b1cc9f1342831538e50f4526a4e0c21a
lyingparachute <igor1501@o2.pl>
2023-01-04 19:29:22 +0100

    Add custom search method

--- src/main/java/com/example/classroom/repository/SubjectRepository.java
+++ src/main/java/com/example/classroom/repository/SubjectRepository.java
@@ -1,7 +1,16 @@
 package com.example.classroom.repository;
 
 import com.example.classroom.entity.Subject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+
+    @Query("select s from Subject s where s.name like %?1%")
+    Page<Subject> findAllByName(String name);
+    @Query("select s from Subject s where s.name like %?1%")
+    Page<Subject> findAllByName(String name, Pageable pageable);
+
 }
