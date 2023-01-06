f06867b9babae0a170b6faf24eb02b632ec19d47
lyingparachute <igor1501@o2.pl>
2023-01-06 17:37:04 +0100

    Change order of imports

--- src/main/resources/static/js/subjects.js
+++ src/main/resources/static/js/subjects.js
@@ -5,20 +5,19 @@ $(document).ready(function () {
     $("#btnClear").on("click", function (e) {
         e.preventDefault();
         $("#name").text("");
-        window.location = "/dashboard/subjects";
+        window.location = "/dashboard/students";
     });
 
     $('.delBtn').on('click', function (event) {
         event.preventDefault();
         let link = $(this);
-        let name = link.attr("name");
+        let studentName = link.attr("name");
         let href = $(this).attr('href');
         $('#deleteModal #delRef').attr('href', href);
-        $("#confirmText").html("Are you sure you want to delete subject with name \<strong\>" + name + "\<\/strong\>? This action cannot be undone and you will be unable to recover any data. ");
+        $("#confirmText").html("Are you sure you want to delete student with name \<strong\>" + studentName + "\<\/strong\>? This action cannot be undone and you will be unable to recover any data. ");
     });
 });
 
 function changePageSize() {
     $("#studentForm").submit();
 }
-
