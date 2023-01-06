91cb2f2e4bb0abb2d85507af0bcd0c68a25ac0c8
lyingparachute <igor1501@o2.pl>
2023-01-06 17:59:30 +0100

    Add methods for subject controller

--- src/main/java/com/example/classroom/controller/SubjectController.java
+++ src/main/java/com/example/classroom/controller/SubjectController.java
@@ -2,17 +2,17 @@ package com.example.classroom.controller;
 
 
 import com.example.classroom.dto.SubjectDto;
+import com.example.classroom.entity.Subject;
 import com.example.classroom.service.SubjectService;
 import com.example.classroom.service.TeacherService;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -33,7 +33,7 @@ public class SubjectController {
     public String getSubjects(@RequestParam(required = false) String name,
                               @RequestParam(defaultValue = "1") int page,
                               @RequestParam(defaultValue = "6") int size,
-                              @RequestParam(defaultValue = "name") String sortField,
+                              @RequestParam(defaultValue = "id") String sortField,
                               @RequestParam(defaultValue = "asc") String sortDir,
                               Model model) {
         Page<SubjectDto> pageSubjects;
@@ -43,7 +43,7 @@ public class SubjectController {
             pageSubjects = subjectService.findByNamePaginated(page, size, sortField, sortDir, name);
             model.addAttribute("name", name);
         }
-        List<SubjectDto> students = pageSubjects.getContent();
+        List<SubjectDto> subjects = pageSubjects.getContent();
         int firstItemShownOnPage = 1;
         int lastItemShownOnPage;
         if (page == 1 && pageSubjects.getTotalElements() <= size) {
@@ -58,7 +58,7 @@ public class SubjectController {
             lastItemShownOnPage = size * (page - 1) + size;
         }
 
-        model.addAttribute("subjects", students);
+        model.addAttribute("subjects", subjects);
         model.addAttribute("currentPage", pageSubjects.getNumber() + 1);
         model.addAttribute("totalPages", pageSubjects.getTotalPages());
         model.addAttribute("totalItems", pageSubjects.getTotalElements());
@@ -83,5 +83,45 @@ public class SubjectController {
         model.addAttribute("subject", new SubjectDto());
         model.addAttribute("teachers", teacherService.fetchAll());
         return "subject/subject-form";
+    }
+
+    @PostMapping(value = "new")
+    public String createSubject(@Valid @ModelAttribute("subject") Subject subject, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("teachers", teacherService.fetchAll());
+            return "subject/subject-form";
+        }
+        subjectService.create(mapper.map(subject, SubjectDto.class));
+        return "subject/subject-create-success";
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteSubject(@PathVariable Long id) {
+        subjectService.remove(id);
+        return "redirect:/dashboard/subjects";
+    }
+
+    @GetMapping("edit/{id}")
+    public String editSubjectForm(@PathVariable Long id, Model model) {
+        SubjectDto dto = subjectService.fetchById(id);
+        model.addAttribute("subject", dto);
+        model.addAttribute("teachers", teacherService.fetchAll());
+        return "subject/subject-edit-form";
+    }
+
+    @PostMapping(value = "update")
+    public String editSubject(@Valid @ModelAttribute("subject") Subject subject, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("teachers", teacherService.fetchAll());
+            return "subject/subject-edit-form";
+        }
+
+        SubjectDto updated = subjectService.update(mapper.map(subject, SubjectDto.class));
+        if (updated == null) {
+            return "error/404";
+        }
+        model.addAttribute("subject", updated);
+
+        return "subject/subject-edit-success";
     }
 }
