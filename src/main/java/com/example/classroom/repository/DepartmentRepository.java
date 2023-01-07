99ffd1f8b6288b4f9b3446cdec35c7d562d0e8f6
lyingparachute <igor1501@o2.pl>
2023-01-07 18:18:03 +0100

    Add custom repo methods

--- src/main/java/com/example/classroom/repository/DepartmentRepository.java
+++ src/main/java/com/example/classroom/repository/DepartmentRepository.java
@@ -1,7 +1,15 @@
 package com.example.classroom.repository;
 
 import com.example.classroom.entity.Department;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    List<Department> findAllByName(String name);
+
+    Page<Department> findAllByName(String name, Pageable pageable);
 }
