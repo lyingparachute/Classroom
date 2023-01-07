d36e71b53668034792d6c514dcdc3c589231cd97
lyingparachute <igor1501@o2.pl>
2023-01-07 18:08:33 +0100

    Correct errors

--- src/main/resources/import/fieldsOfStudy.sql
+++ src/main/resources/import/fieldsOfStudy.sql
@@ -23,7 +23,7 @@ INSERT INTO fields_of_study (id, name, `level_of_education`, `study_mode`, `obta
 INSERT INTO fields_of_study (id, name, `level_of_education`, `study_mode`, `obtained_title`, department_id) VALUES(18, 'Oceanotechnika', 'SECOND', 'FT', 'MGR_ENG', 7);
 INSERT INTO fields_of_study (id, name, `level_of_education`, `study_mode`, `obtained_title`, department_id) VALUES(19, 'Oceanotechnika', 'SECOND', 'PT', 'MGR_ENG', 7);
 INSERT INTO fields_of_study (id, name, `level_of_education`, `study_mode`, `obtained_title`, department_id) VALUES(20, 'Technologie kosmiczne i satelitarne', 'SECOND', 'FT', 'MGR_ENG', 5);
-INSERT INTO fields_of_study (id, name, `level_of_education`, `study_mode`, `obtained_title`, department_id) VALUES(21, 'Transport i logistyka', 'SECOND', 'FT', 'MGR_ENG'6);
+INSERT INTO fields_of_study (id, name, `level_of_education`, `study_mode`, `obtained_title`, department_id) VALUES(21, 'Transport i logistyka', 'SECOND', 'FT', 'MGR_ENG', 6);
 INSERT INTO fields_of_study (id, name, `level_of_education`, `study_mode`, `obtained_title`, department_id) VALUES(26, 'Automatyka, cybernetyka i robotyka', 'SECOND', 'FT', 'MGR_ENG', 4);
 INSERT INTO fields_of_study (id, name, `level_of_education`, `study_mode`, `obtained_title`, department_id) VALUES(27, 'Elektronika i telekomunikacja', 'SECOND', 'FT', 'MGR_ENG', 1);
 INSERT INTO fields_of_study (id, name, `level_of_education`, `study_mode`, `obtained_title`, department_id) VALUES(28, 'Informatyka', 'SECOND', 'FT', 'MGR_ENG', 1);
