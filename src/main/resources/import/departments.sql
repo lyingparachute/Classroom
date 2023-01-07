af03e0957e5fbea6050fc7b2d44987a16fb57820
lyingparachute <igor1501@o2.pl>
2023-01-07 16:12:08 +0100

    Add more test data

---
+++ src/main/resources/import/departments.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(1, 'Wydział Elektroniki, Telekomunikacji i Informatyki', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 1);
+INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(2, 'Wydział Architektury', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 11);
+INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(3, 'Wydział Chemiczny', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 8);
+INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(4, 'Wydział Elektrotechniki i Automatyki', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 3);
+INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(5, 'WWydział Fizyki Technicznej i Matematyki Stosowanej', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 10);
+INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(6, 'Wydział Inżynierii Lądowej i Środowiska', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 2);
+INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(7, 'Wydział Inżynierii Mechanicznej i Okrętownictwa', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 6);
+INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(8, 'Wydział Zarządzania i Ekonomii, Telekomunikacji i Informatyki', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 4);
