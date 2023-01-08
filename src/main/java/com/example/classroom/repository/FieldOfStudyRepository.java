733f9ab423caadf663ab81f1f2056139181719ed
lyingparachute <igor1501@o2.pl>
2023-01-08 09:49:28 +0100

    Add custom method

--- src/main/java/com/example/classroom/repository/FieldOfStudyRepository.java
+++ src/main/java/com/example/classroom/repository/FieldOfStudyRepository.java
@@ -1,7 +1,13 @@
 package com.example.classroom.repository;
 
 import com.example.classroom.entity.FieldOfStudy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FieldOfStudyRepository extends JpaRepository<FieldOfStudy, Long> {
+    List<FieldOfStudy> findAllByName(String name);
+    Page<FieldOfStudy> findAllByName(String name, Pageable pageable);
 }
