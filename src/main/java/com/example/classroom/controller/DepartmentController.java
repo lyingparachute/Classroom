8b32d17ce579049286ced256eab3a11fbaaeebab
lyingparachute <igor1501@o2.pl>
2023-01-08 10:19:20 +0100

    Add views for field of study and department

--- src/main/java/com/example/classroom/controller/DepartmentController.java
+++ src/main/java/com/example/classroom/controller/DepartmentController.java
@@ -1,4 +1,10 @@
 package com.example.classroom.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("dashboard/departments")
 public class DepartmentController {
+
 }
