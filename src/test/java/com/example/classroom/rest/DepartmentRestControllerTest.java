4d03c691b8fa1e18704d6ed25a1e6edc6839938f
lyingparachute <igor1501@o2.pl>
2023-01-08 09:49:00 +0100

    Add department rest controller

---
+++ src/test/java/com/example/classroom/rest/DepartmentRestControllerTest.java
@@ -0,0 +1,85 @@
+package com.example.classroom.rest;
+
+import com.example.classroom.entity.Teacher;
+import com.example.classroom.repository.DepartmentRepository;
+import com.example.classroom.repository.util.InitData;
+import com.example.classroom.repository.util.InitDepartment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DepartmentRestControllerTest {
+    @Autowired
+    private InitData initData;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    int randomServerPort;
+
+    @Autowired
+    InitDepartment initDepartment;
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    @BeforeEach
+    public void setup() {
+        initData.cleanUp();
+        initDepartment.cleanUp();
+    }
+
+    @Test
+    void shouldGetAllDepartments() throws URISyntaxException {
+        //given
+        Teacher teacher = initData.createTeacherTwo(List.of());
+        initDepartment.createDepartmentOne(teacher);
+        //when
+        URI url = createURL("/api/subjects/");
+        ResponseEntity<Set> response = restTemplate.getForEntity(url, Set.class);
+        //then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Set actual = response.getBody();
+        assertThat(actual).isNotNull();
+        assertThat(actual).isNotEmpty();
+        assertThat(actual).size().isEqualTo(8);
+    }
+
+    @Test
+    void createDepartment() throws URISyntaxException {
+    }
+
+    @Test
+    void getDepartment() throws URISyntaxException {
+    }
+
+    @Test
+    void putDepartment() throws URISyntaxException {
+    }
+
+    @Test
+    void deleteDepartment() throws URISyntaxException {
+    }
+
+    @Test
+    void deleteAllDepartments() throws URISyntaxException {
+    }
+
+    private URI createURL(String path) throws URISyntaxException {
+        return new URI("http://localhost:" + randomServerPort + path);
+    }
+}
