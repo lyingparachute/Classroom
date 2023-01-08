51344f78f5864ba266227e9ac8b8152075dd7ab6
lyingparachute <igor1501@o2.pl>
2023-01-08 15:28:08 +0100

    Add department service class

---
+++ src/test/java/com/example/classroom/service/DepartmentServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.classroom.service;
+
+import org.junit.jupiter.api.Test;
+
+class DepartmentServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void fetchAll() {
+    }
+
+    @Test
+    void fetchAllPaginated() {
+    }
+
+    @Test
+    void fetchById() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findByNamePaginated() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+}
