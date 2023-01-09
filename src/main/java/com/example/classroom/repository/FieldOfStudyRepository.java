51441080d1a2c197658e72e2a8ec8c808a0e95da
lyingparachute <igor1501@o2.pl>
2023-01-09 19:12:29 +0100

    Refactor custom methods

--- src/main/java/com/example/classroom/repository/FieldOfStudyRepository.java
+++ src/main/java/com/example/classroom/repository/FieldOfStudyRepository.java
@@ -4,10 +4,13 @@ import com.example.classroom.entity.FieldOfStudy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface FieldOfStudyRepository extends JpaRepository<FieldOfStudy, Long> {
+    @Query("select f from FieldOfStudy f where f.name like %?1%")
     List<FieldOfStudy> findAllByName(String name);
+    @Query("select f from FieldOfStudy f where f.name like %?1%")
     Page<FieldOfStudy> findAllByName(String name, Pageable pageable);
 }
