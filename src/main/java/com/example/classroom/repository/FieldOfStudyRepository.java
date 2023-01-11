66ade3090f0f8999885957056ed0227ec1ad796e
lyingparachute <igor1501@o2.pl>
2023-01-11 11:44:13 +0100

    Fix bug with case-sensitive search

--- src/main/java/com/example/classroom/repository/FieldOfStudyRepository.java
+++ src/main/java/com/example/classroom/repository/FieldOfStudyRepository.java
@@ -4,13 +4,11 @@ import com.example.classroom.entity.FieldOfStudy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface FieldOfStudyRepository extends JpaRepository<FieldOfStudy, Long> {
-    @Query("select f from FieldOfStudy f where f.name like %?1%")
-    List<FieldOfStudy> findAllByName(String name);
-    @Query("select f from FieldOfStudy f where f.name like %?1%")
-    Page<FieldOfStudy> findAllByName(String name, Pageable pageable);
+
+    List<FieldOfStudy> findAllByNameContainingIgnoreCase(String name);
+    Page<FieldOfStudy> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
 }
