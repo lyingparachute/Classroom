bc6a8633accb881da4d0276f78e2217d9545fc39
lyingparachute <igor1501@o2.pl>
2023-01-21 08:12:42 +0100

    Delete unused method

--- src/main/java/com/example/classroom/repository/SubjectRepository.java
+++ src/main/java/com/example/classroom/repository/SubjectRepository.java
@@ -1,11 +1,9 @@
 package com.example.classroom.repository;
 
 import com.example.classroom.entity.Subject;
-import com.example.classroom.enums.Semester;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,7 +11,4 @@ public interface SubjectRepository extends JpaRepository<Subject, Long> {
     List<Subject> findAllByNameContainingIgnoreCase(String name);
 
     Page<Subject> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
-
-    @Query("select s from Subject s where s.fieldOfStudy.id = ?1 and s.semester = ?2")
-    List<Subject> findAllByFieldOfStudy(Long id, Semester semester);
 }
