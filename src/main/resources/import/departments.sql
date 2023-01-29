c020434081594033a73e0c833cb9ec6b18493451
lyingparachute <igor1501@o2.pl>
2023-01-29 19:19:15 +0100

    Change name of department

--- src/main/resources/import/departments.sql
+++ src/main/resources/import/departments.sql
@@ -1,8 +1,8 @@
-INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(1, 'Wydział Elektroniki, Telekomunikacji i Informatyki', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 1);
-INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(2, 'Wydział Architektury', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 11);
-INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(3, 'Wydział Chemiczny', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 8);
-INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(4, 'Wydział Elektrotechniki i Automatyki', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 3);
-INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(5, 'Wydział Fizyki Technicznej i Matematyki Stosowanej', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 10);
-INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(6, 'Wydział Inżynierii Lądowej i Środowiska', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 2);
-INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(7, 'Wydział Inżynierii Mechanicznej i Okrętownictwa', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 6);
-INSERT INTO departments (id, name, address, tel_number, dean_id) VALUES(8, 'Wydział Zarządzania i Ekonomii, Telekomunikacji i Informatyki', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 4);
+insert into departments (id, name, address, tel_number, dean_id) values(1, 'Wydział Elektroniki, Telekomunikacji i Informatyki', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 1);
+insert into departments (id, name, address, tel_number, dean_id) values(2, 'Wydział Architektury', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 11);
+insert into departments (id, name, address, tel_number, dean_id) values(3, 'Wydział Chemiczny', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 8);
+insert into departments (id, name, address, tel_number, dean_id) values(4, 'Wydział Elektrotechniki i Automatyki', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 3);
+insert into departments (id, name, address, tel_number, dean_id) values(5, 'Wydział Fizyki Technicznej i Matematyki Stosowanej', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 10);
+insert into departments (id, name, address, tel_number, dean_id) values(6, 'Wydział Inżynierii Lądowej i Środowiska', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 2);
+insert into departments (id, name, address, tel_number, dean_id) values(7, 'Wydział Inżynierii Mechanicznej i Okrętownictwa', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 6);
+insert into departments (id, name, address, tel_number, dean_id) values(8, 'Wydział Zarządzania i Ekonomii', 'ul. Gabriela Narutowicza 11/12 80-233 Gdańsk', '123456789', 4);
