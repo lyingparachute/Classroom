ca2d808db1a1c84473d8cf10332c5a93b2450e4e
lyingparachute <igor1501@o2.pl>
2023-01-29 20:10:54 +0100

    Add all-departments view

--- src/main/java/com/example/classroom/controller/DepartmentController.java
+++ src/main/java/com/example/classroom/controller/DepartmentController.java
@@ -1,10 +1,25 @@
 package com.example.classroom.controller;
 
+import com.example.classroom.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.nio.file.Path;
 
 @Controller
 @RequestMapping("dashboard/departments")
+@RequiredArgsConstructor
 public class DepartmentController {
 
+    private final DepartmentService service;
+
+    @GetMapping()
+    public String getAllDepartments(Model model) {
+        model.addAttribute("departments", service.fetchAll());
+        model.addAttribute("imagesPath", Path.of("/img").resolve("departments"));
+        return "department/all-departments";
+    }
 }
