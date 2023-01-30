830ef97156b3f671c1f851c8382de93811c61fd4
lyingparachute <igor1501@o2.pl>
2023-01-30 19:56:17 +0100

    Add endpoint and view for single department

--- src/main/java/com/example/classroom/controller/DepartmentController.java
+++ src/main/java/com/example/classroom/controller/DepartmentController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.nio.file.Path;
@@ -16,10 +17,25 @@ public class DepartmentController {
 
     private final DepartmentService service;
 
+    private static void addAttributeImagesPath(Model model, String directory) {
+        model.addAttribute("imagesPath", Path.of("/img").resolve(directory));
+    }
+
     @GetMapping()
     public String getAllDepartments(Model model) {
         model.addAttribute("departments", service.fetchAll());
-        model.addAttribute("imagesPath", Path.of("/img").resolve("departments"));
+        addAttributeImagesPath(model, "departments");
         return "department/all-departments";
+    }
+
+    @GetMapping("{id}")
+    public String getDepartment(@PathVariable Long id, Model model) {
+        addAttributeDepartmentFetchById(id, model);
+        addAttributeImagesPath(model, "fields-of-study");
+        return "department/department-view";
+    }
+
+    private void addAttributeDepartmentFetchById(Long id, Model model) {
+        model.addAttribute("department", service.fetchById(id));
     }
 }
