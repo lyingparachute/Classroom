1ae9a85ef54e166d58657f725f8b58b7a16c524d
lyingparachute <igor1501@o2.pl>
2023-02-20 19:50:51 +0100

    Optimize methods

--- src/main/java/com/example/classroom/service/FieldOfStudyService.java
+++ src/main/java/com/example/classroom/service/FieldOfStudyService.java
@@ -11,18 +11,14 @@ import com.example.classroom.enums.Semester;
 import com.example.classroom.repository.FieldOfStudyRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static java.util.Map.entry;
 
 @Service
 @RequiredArgsConstructor
@@ -41,12 +37,6 @@ public class FieldOfStudyService {
     public List<FieldOfStudyDto> fetchAll() {
         List<FieldOfStudy> all = repository.findAll();
         return all.stream().map(fieldOfStudy -> mapper.map(fieldOfStudy, FieldOfStudyDto.class)).toList();
-    }
-
-    private static Sort getSortOrder(String sortField, String sortDirection) {
-        return sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ?
-                Sort.by(sortField).ascending() :
-                Sort.by(sortField).descending();
     }
 
     public FieldOfStudyDto fetchById(Long id) {
@@ -69,63 +59,20 @@ public class FieldOfStudyService {
         repository.deleteAll();
     }
 
-    public List<FieldOfStudyDto> findByName(String searched) {
-        List<FieldOfStudy> found = repository.findAllByNameContainingIgnoreCase(searched);
-        return found.stream().map(s -> mapper.map(s, FieldOfStudyDto.class)).toList();
-    }
-
-    public Page<FieldOfStudyDto> findByNamePaginated(int pageNo,
-                                                     int pageSize,
-                                                     String sortField,
-                                                     String sortDirection,
-                                                     String searched) {
-        Sort sort = getSortOrder(sortField, sortDirection);
-        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
-        Page<FieldOfStudy> all = repository.findAllByNameContainingIgnoreCase(searched, pageable);
-        return all.map(fieldOfStudy -> mapper.map(fieldOfStudy, FieldOfStudyDto.class));
-    }
-
-    public Page<FieldOfStudyDto> fetchAllPaginated(int pageNo,
-                                                   int pageSize,
-                                                   String sortField,
-                                                   String sortDirection) {
-        Sort sort = getSortOrder(sortField, sortDirection);
-        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
-        Page<FieldOfStudy> all = repository.findAll(pageable);
-        return all.map(fieldOfStudy -> mapper.map(fieldOfStudy, FieldOfStudyDto.class));
-    }
-
     public Map<Semester, List<Subject>> fetchAllSubjectsFromFieldOfStudyGroupedBySemesters(Long fieldOfStudyId) {
         List<Subject> subjects = repository.findAllSubjectsFromFieldOfStudy(fieldOfStudyId);
-        return Map.ofEntries(
-                entry(Semester.FIRST, filterSubjectsBySemester(subjects, Semester.FIRST).toList()),
-                entry(Semester.SECOND, filterSubjectsBySemester(subjects, Semester.SECOND).toList()),
-                entry(Semester.THIRD, filterSubjectsBySemester(subjects, Semester.THIRD).toList()),
-                entry(Semester.FOURTH, filterSubjectsBySemester(subjects, Semester.FOURTH).toList()),
-                entry(Semester.FIFTH, filterSubjectsBySemester(subjects, Semester.FIFTH).toList()),
-                entry(Semester.SIXTH, filterSubjectsBySemester(subjects, Semester.SIXTH).toList()),
-                entry(Semester.SEVENTH, filterSubjectsBySemester(subjects, Semester.SEVENTH).toList())
-        );
+        return Arrays.stream(Semester.values()).collect(Collectors.toMap(
+                s -> s,
+                s -> filterSubjectsBySemester(subjects, s).toList()
+        ));
     }
 
     public Map<Semester, Integer> calculateHoursInEachSemesterFromFieldOfStudy(Long fieldOfStudyId) {
         List<Subject> subjects = repository.findAllSubjectsFromFieldOfStudy(fieldOfStudyId);
-        return Map.ofEntries(
-                entry(Semester.FIRST, filterSubjectsBySemester(subjects, Semester.FIRST)
-                        .mapToInt(Subject::getHoursInSemester).sum()),
-                entry(Semester.SECOND, filterSubjectsBySemester(subjects, Semester.SECOND)
-                        .mapToInt(Subject::getHoursInSemester).sum()),
-                entry(Semester.THIRD, filterSubjectsBySemester(subjects, Semester.THIRD)
-                        .mapToInt(Subject::getHoursInSemester).sum()),
-                entry(Semester.FOURTH, filterSubjectsBySemester(subjects, Semester.FOURTH)
-                        .mapToInt(Subject::getHoursInSemester).sum()),
-                entry(Semester.FIFTH, filterSubjectsBySemester(subjects, Semester.FIFTH)
-                        .mapToInt(Subject::getHoursInSemester).sum()),
-                entry(Semester.SIXTH, filterSubjectsBySemester(subjects, Semester.SIXTH)
-                        .mapToInt(Subject::getHoursInSemester).sum()),
-                entry(Semester.SEVENTH, filterSubjectsBySemester(subjects, Semester.SEVENTH)
-                        .mapToInt(Subject::getHoursInSemester).sum())
-        );
+        return Arrays.stream(Semester.values()).collect(Collectors.toMap(
+                s -> s,
+                s -> filterSubjectsBySemester(subjects, s).mapToInt(Subject::getHoursInSemester).sum()
+        ));
     }
 
     private Stream<Subject> filterSubjectsBySemester(List<Subject> subjects, Semester semester) {
@@ -214,13 +161,6 @@ public class FieldOfStudyService {
         return imagePath.resolve(Objects.requireNonNullElse(imageName, "default.jpg")).toString();
     }
 
-    public Map<LevelOfEducation, List<FieldOfStudyDto>> fetchAllGroupedByLevelOfEducation() {
-        return Map.ofEntries(
-                entry(LevelOfEducation.FIRST, fetchAllByLevelOfEducationSortedByName(LevelOfEducation.FIRST)),
-                entry(LevelOfEducation.SECOND, fetchAllByLevelOfEducationSortedByName(LevelOfEducation.SECOND))
-        );
-    }
-
     public List<FieldOfStudyDto> fetchAllByLevelOfEducationSortedByName(LevelOfEducation levelOfEducation) {
         return repository.findAllByLevelOfEducation(levelOfEducation, Sort.by(Sort.Direction.ASC, "name"))
                 .stream().map(fieldOfStudy -> mapper.map(fieldOfStudy, FieldOfStudyDto.class)).toList();
@@ -235,6 +175,25 @@ public class FieldOfStudyService {
                     .stream().map(f -> mapper.map(f, FieldOfStudyDto.class)).toList());
         }
         return map;
+    }
+
+    public List<FieldOfStudyDto> fetchAllWithNoDepartment() {
+        return repository.findAll().stream()
+                .filter(fieldOfStudy -> fieldOfStudy.getDepartment() == null)
+                .map(fieldOfStudy -> mapper.map(fieldOfStudy, FieldOfStudyDto.class))
+                .toList();
+    }
+
+    public List<FieldOfStudyDto> fetchAllWithGivenDepartmentDtoOrNoDepartment(DepartmentDto dto) {
+        return Stream.concat(
+                repository.findAll().stream()
+                        .filter(fieldOfStudy -> fieldOfStudy.getDepartment() == null)
+                        .map(fieldOfStudy -> mapper.map(fieldOfStudy, FieldOfStudyDto.class)),
+                repository.findAll().stream()
+                        .filter(fieldOfStudy -> fieldOfStudy.getDepartment() != null)
+                        .filter(fieldOfStudy -> Objects.equals(fieldOfStudy.getDepartment().getId(), dto.getId()))
+                        .map(fieldOfStudy -> mapper.map(fieldOfStudy, FieldOfStudyDto.class))
+        ).toList();
     }
 
     private void removeReferencingObjects(FieldOfStudy fieldOfStudy) {
@@ -255,24 +214,5 @@ public class FieldOfStudyService {
     private void removeStudents(FieldOfStudy fieldOfStudy) {
         Set<Student> students = fieldOfStudy.getStudents();
         students.forEach(fieldOfStudy::removeStudent);
-    }
-
-    public List<FieldOfStudyDto> fetchAllWithNoDepartment() {
-        return repository.findAll().stream()
-                .filter(fieldOfStudy -> fieldOfStudy.getDepartment() == null)
-                .map(fieldOfStudy -> mapper.map(fieldOfStudy, FieldOfStudyDto.class))
-                .toList();
-    }
-
-    public List<FieldOfStudyDto> fetchAllWithGivenDepartmentDtoOrNoDepartment(DepartmentDto dto) {
-        return Stream.concat(
-                repository.findAll().stream()
-                        .filter(fieldOfStudy -> fieldOfStudy.getDepartment() == null)
-                        .map(fieldOfStudy -> mapper.map(fieldOfStudy, FieldOfStudyDto.class)),
-                repository.findAll().stream()
-                        .filter(fieldOfStudy -> fieldOfStudy.getDepartment() != null)
-                        .filter(fieldOfStudy -> Objects.equals(fieldOfStudy.getDepartment().getId(), dto.getId()))
-                        .map(fieldOfStudy -> mapper.map(fieldOfStudy, FieldOfStudyDto.class))
-        ).toList();
     }
 }
