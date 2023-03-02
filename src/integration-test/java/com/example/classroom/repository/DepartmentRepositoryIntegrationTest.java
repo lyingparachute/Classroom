df8f5bfad543af1c202c9a46c6785be4263e0874
lyingparachute <igor1501@o2.pl>
2023-03-02 20:24:53 +0100

    Add methods for initializing test data

--- src/integration-test/java/com/example/classroom/repository/DepartmentRepositoryIntegrationTest.java
+++ src/integration-test/java/com/example/classroom/repository/DepartmentRepositoryIntegrationTest.java
@@ -1,30 +1,37 @@
 package com.example.classroom.repository;
 
 import com.example.classroom.entity.Department;
-import com.example.classroom.repository.util.IntegrationTestsInitData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@ActiveProfiles("test")
 class DepartmentRepositoryIntegrationTest {
 
     @Autowired
-    DepartmentRepository underTest;
+    DepartmentRepository repository;
 
     @Autowired
-    IntegrationTestsInitData initData;
+    EntityManager entityManager;
 
     @BeforeEach
-    void setup() {
-        initData.cleanUp();
+    void setUp() {
+        repository.findAll().forEach(department -> department.getFieldsOfStudy()
+                .forEach(department::removeFieldOfStudy));
+        repository.deleteAll();
+        initTestData();
+    }
+
+    @Test
+    void injectedComponentsAreNotNull() {
+        assertThat(entityManager).isNotNull();
+        assertThat(repository).isNotNull();
     }
 
     @Test
@@ -41,5 +48,44 @@ class DepartmentRepositoryIntegrationTest {
         Department actualDepartment = actual.get(0);
         assertThat(actualDepartment).isInstanceOf(Department.class);
         assertThat(actualDepartment.getName()).isEqualTo(expected.getName());
+    }
+
+    private void initTestData() {
+        createDepartmentMechaniczny();
+        createDepartmentMechatroniczny();
+        createDepartmentArchitektury();
+        createDepartmentChemiczny();
+    }
+
+    private void createDepartmentMechaniczny() {
+        Department department = new Department();
+        department.setName("Wydział Mechaniczny");
+        department.setAddress("ul. Mechaniczna 1, 24-192 Oborniki");
+        department.setTelNumber("987739874");
+        entityManager.persist(department);
+    }
+
+    private void createDepartmentMechatroniczny() {
+        Department department = new Department();
+        department.setName("Wydział Architektury");
+        department.setAddress("ul. Mechatroniczna 12, 99-112 Gdynia");
+        department.setTelNumber("333444555");
+        entityManager.persist(department);
+    }
+
+    private void createDepartmentArchitektury() {
+        Department department = new Department();
+        department.setName("Wydział Architektury");
+        department.setAddress("ul. Jabłoniowa 34, 11-112 Stalowa Wola");
+        department.setTelNumber("321321321");
+        entityManager.persist(department);
+    }
+
+    private void createDepartmentChemiczny() {
+        Department department = new Department();
+        department.setName("Wydział Chemiczny");
+        department.setAddress("ul. Broniewicza 115, 00-245 Kęty");
+        department.setTelNumber("987654321");
+        entityManager.persist(department);
     }
 }
