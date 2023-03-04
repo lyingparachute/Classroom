2d8a974e75868739560e7fd61245a2835f2428a7
lyingparachute <igor1501@o2.pl>
2023-03-04 15:02:05 +0100

    Add test class for subject repository

---
+++ src/integration-test/java/com/example/classroom/repository/SubjectRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.example.classroom.repository;
+
+import com.example.classroom.entity.Subject;
+import com.example.classroom.enums.Semester;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.EntityManager;
+
+@DataJpaTest
+class SubjectRepositoryTest {
+
+    @Autowired
+    SubjectRepository repository;
+
+    @Autowired
+    EntityManager entityManager;
+
+
+    @Nested
+    class FindAllByNameContainingIgnoreCase {
+
+        @Test
+        void returnsEmptyListOfSubjects_givenNonExistingName() {
+        }
+
+        @Test
+        void returnsListOfSubjects_givenName() {
+        }
+
+        @Test
+        void returnsListOfSubjects_givenNameAndPageable() {
+        }
+
+    }
+
+
+    @Nested
+    class FindAllBySemester {
+
+        @Test
+        void returnsEmptyListOfSubjects_givenNonExistingSemester() {
+
+        }
+
+        @Test
+        void returnsListOfSubjects_givenSemester() {
+
+        }
+    }
+
+    private Subject createSubject1() {
+        Subject subject = new Subject();
+        subject.setName("Mathematics");
+        subject.setDescription("Calculating integrals");
+        subject.setSemester(Semester.FIFTH);
+        subject.setHoursInSemester(100);
+        entityManager.persist(subject);
+        return subject;
+    }
+
+    private Subject createSubject2() {
+        Subject subject = new Subject();
+        subject.setName("Art");
+        subject.setDescription("Painting");
+        subject.setSemester(Semester.SECOND);
+        subject.setHoursInSemester(120);
+        entityManager.persist(subject);
+        return subject;
+    }
+
+    private Subject createSubject3() {
+        Subject subject = new Subject();
+        subject.setName("Science");
+        subject.setDescription("General Science");
+        subject.setSemester(Semester.FIRST);
+        subject.setHoursInSemester(150);
+        entityManager.persist(subject);
+        return subject;
+    }
+}
