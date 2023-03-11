fe9dcb2f634f39013a27ede30c2cffd29971cf5d
lyingparachute <igor1501@o2.pl>
2023-03-11 19:14:11 +0100

    Rename class

--- src/main/java/com/example/classroom/config/ModelMapperConfig.java
+++ src/main/java/com/example/classroom/config/ModelMapperConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Beans {
+public class ModelMapperConfig {
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 }
