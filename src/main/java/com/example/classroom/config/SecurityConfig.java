86807e825a366be506a73b0b73d5c47a40516ebb
lyingparachute <igor1501@o2.pl>
2023-03-12 21:00:22 +0100

    Edit security config - add cusotm login page

--- src/main/java/com/example/classroom/config/SecurityConfig.java
+++ src/main/java/com/example/classroom/config/SecurityConfig.java
@@ -11,17 +11,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String HOME_PAGE = "/";
     private static final String DASHBOARD_PAGE = "/dashboard";
+    private static final String SIGN_IN_PAGE = "/login";
+    private static final String SIGN_IN_API = "/api/sign-in";
+    private static final String SIGN_OUT_API = "/api/sign-out";
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/css/**", "/js/**", "/assets/**", "/img/home/**", "/webjars/**",
-                        HOME_PAGE).permitAll()
+                        HOME_PAGE,
+                        SIGN_IN_PAGE,
+                        SIGN_IN_API).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .defaultSuccessUrl(DASHBOARD_PAGE, true);
+                .loginPage(SIGN_IN_PAGE)
+                .loginProcessingUrl(SIGN_IN_API)
+                .defaultSuccessUrl(DASHBOARD_PAGE, true)
+                .failureUrl(SIGN_IN_PAGE + "?error")
+                .and()
+                .logout()
+                .logoutUrl(SIGN_OUT_API)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl(HOME_PAGE);
     }
 }
 
