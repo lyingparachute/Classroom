a228bf1c3b87f599da7b9a92feb706933464361d
lyingparachute <igor1501@o2.pl>
2023-03-14 21:19:53 +0100

    Add in-memory user and admin

--- src/main/java/com/example/classroom/config/SecurityConfig.java
+++ src/main/java/com/example/classroom/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.example.classroom.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -36,6 +39,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID")
                 .logoutSuccessUrl(HOME_PAGE);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password(encoder.encode("user"))
+                .roles();
     }
 }
 
