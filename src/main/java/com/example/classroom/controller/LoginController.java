3aab10a07f12575080bb7a6bc46c3c3d9e18b746
lyingparachute <igor1501@o2.pl>
2023-03-15 12:30:50 +0100

    Add password-reset endpoint and sign-up

--- src/main/java/com/example/classroom/controller/LoginController.java
+++ src/main/java/com/example/classroom/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.classroom.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -18,12 +19,22 @@ public class LoginController {
     }
 
     @GetMapping("/sign-up")
-    public String signUp() {
+    public String getSignUpPage() {
         return LOGIN_FOLDER + "sign-up";
     }
 
-    @GetMapping("password-reset")
-    public String passwordReset() {
+    @PostMapping("/api/sign-up")
+    public String signUp() {
+        return LOGIN_FOLDER + "sign-in";
+    }
+
+    @GetMapping("/password/reset")
+    public String getPasswordResetPage() {
         return LOGIN_FOLDER + "password-reset";
+    }
+
+    @PostMapping("/password/reset")
+    public String resetPassword() {
+        return LOGIN_FOLDER + "sign-in";
     }
 }
