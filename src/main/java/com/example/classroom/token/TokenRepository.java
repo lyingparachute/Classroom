4d49448f86bf0ec026f57bc255ac0ad2569d14a4
lyingparachute <igor1501@o2.pl>
2023-03-17 16:31:08 +0100

    Add Token entity

---
+++ src/main/java/com/example/classroom/token/TokenRepository.java
@@ -0,0 +1,8 @@
+package com.example.classroom.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Long, Token> {
+}
