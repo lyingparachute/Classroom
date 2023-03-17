b37e2c1299303239db1b2e8e3dbe68859d72cd94
lyingparachute <igor1501@o2.pl>
2023-03-17 15:29:54 +0100

    Implement methods in service

--- src/main/java/com/example/classroom/auth/AuthenticationService.java
+++ src/main/java/com/example/classroom/auth/AuthenticationService.java
@@ -1,17 +1,43 @@
 package com.example.classroom.auth;
 
+import com.example.classroom.config.jwt.JwtService;
+import com.example.classroom.model.Student;
+import com.example.classroom.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final StudentRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        //TODO - save user to database
+        var jwtToken = jwtService.generateToken(new Student());
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
-    public Object authenticate(AuthenticationRequest request) {
-        return null;
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                ));
+        Student user = repository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + request.getEmail() + " does not exist in database."));
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
