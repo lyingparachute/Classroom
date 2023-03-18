eff1646c72d3438b9d09195b115bc3c648a1b000
lyingparachute <igor1501@o2.pl>
2023-03-18 11:25:09 +0100

    Create methods for saving tokens upon registering user via API

--- src/main/java/com/example/classroom/auth/AuthenticationService.java
+++ src/main/java/com/example/classroom/auth/AuthenticationService.java
@@ -4,6 +4,9 @@ import com.example.classroom.config.jwt.JwtService;
 import com.example.classroom.enums.RoleEnum;
 import com.example.classroom.model.UserLogin;
 import com.example.classroom.repository.UserRepository;
+import com.example.classroom.token.Token;
+import com.example.classroom.token.TokenRepository;
+import com.example.classroom.token.TokenType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
 
     private final UserRepository repository;
+    private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
@@ -29,6 +33,7 @@ public class AuthenticationService {
         userDetails.setRole(RoleEnum.USER);
         var savedUser = repository.save(userDetails);
         var jwtToken = jwtService.generateToken(savedUser);
+        saveUserToken(savedUser, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
@@ -43,8 +48,32 @@ public class AuthenticationService {
         UserLogin user = repository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new UsernameNotFoundException("User with email " + request.getEmail() + " does not exist in database."));
         var jwtToken = jwtService.generateToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    private void saveUserToken(UserLogin user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+
+    private void revokeAllUserTokens(UserLogin user) {
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
     }
 }
