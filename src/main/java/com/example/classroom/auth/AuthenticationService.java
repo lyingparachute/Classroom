c7a26dfede0a37c4b6dc7958659b721b8a17398d
lyingparachute <igor1501@o2.pl>
2023-03-18 10:17:10 +0100

    Change Student to UserLogin

--- src/main/java/com/example/classroom/auth/AuthenticationService.java
+++ src/main/java/com/example/classroom/auth/AuthenticationService.java
@@ -1,8 +1,9 @@
 package com.example.classroom.auth;
 
 import com.example.classroom.config.jwt.JwtService;
-import com.example.classroom.model.Student;
-import com.example.classroom.repository.StudentRepository;
+import com.example.classroom.enums.RoleEnum;
+import com.example.classroom.model.UserLogin;
+import com.example.classroom.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,19 +15,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    private final StudentRepository repository;
+    private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        //TODO - save user to database
-        Student userDetails = new Student();
+        UserLogin userDetails = new UserLogin();
         userDetails.setFirstName(request.getFirstName());
         userDetails.setLastName(request.getLastName());
         userDetails.setEmail(request.getEmail());
         userDetails.setPassword(passwordEncoder.encode(request.getPassword()));
-        userDetails.setRole("USER");
+        userDetails.setRole(RoleEnum.USER);
         var savedUser = repository.save(userDetails);
         var jwtToken = jwtService.generateToken(savedUser);
         return AuthenticationResponse.builder()
@@ -40,7 +40,7 @@ public class AuthenticationService {
                         request.getEmail(),
                         request.getPassword()
                 ));
-        Student user = repository.findByEmail(request.getEmail())
+        UserLogin user = repository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new UsernameNotFoundException("User with email " + request.getEmail() + " does not exist in database."));
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
