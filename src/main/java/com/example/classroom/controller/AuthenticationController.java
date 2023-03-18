3371e4dd8575d4d639488edd4e89a085017c63c3
lyingparachute <igor1501@o2.pl>
2023-03-18 10:19:29 +0100

    Add register methods

--- src/main/java/com/example/classroom/controller/AuthenticationController.java
+++ src/main/java/com/example/classroom/controller/AuthenticationController.java
@@ -1,11 +1,21 @@
 package com.example.classroom.controller;
 
+import com.example.classroom.model.UserLogin;
+import com.example.classroom.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final UserService service;
+
     public static final String LOGIN_FOLDER = "login/";
 
     @GetMapping("/sign-in")
@@ -14,13 +24,17 @@ public class AuthenticationController {
     }
 
     @GetMapping("/sign-up")
-    public String getSignUpPage() {
+    public String getSignUpPage(Model model) {
+        model.addAttribute("user", new UserLogin());
         return LOGIN_FOLDER + "sign-up";
     }
 
-    @PostMapping("/api/sign-up")
-    public String signUp() {
-        return LOGIN_FOLDER + "sign-in";
+    @PostMapping("/create-user")
+    public String signUp(@ModelAttribute("user") UserLogin user,
+                         RedirectAttributes redirectAttributes) {
+        UserLogin created = service.create(user);
+        redirectAttributes.addFlashAttribute("createSuccess", created);
+        return "redirect:/sign-in";
     }
 
     @GetMapping("/password/reset")
