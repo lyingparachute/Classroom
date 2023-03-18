c3c1a81344043e3afff52d8d43533b0b0495a905
lyingparachute <igor1501@o2.pl>
2023-03-18 10:21:16 +0100

    Change Student to User repo

--- src/main/java/com/example/classroom/service/UserService.java
+++ src/main/java/com/example/classroom/service/UserService.java
@@ -1,21 +1,45 @@
 package com.example.classroom.service;
 
+import com.example.classroom.model.UserLogin;
 import com.example.classroom.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
     private final UserRepository repository;
+    private final ModelMapper mapper;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserLogin create(UserLogin user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return repository.save(user);
+    }
+
+    @Transactional
+    public UserLogin update(UserLogin user) {
+        UserLogin userLogin = loadUserByUsername(user.getEmail());
+        mapper.map(user, userLogin);
+        return repository.save(userLogin);
+    }
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserLogin loadUserByUsername(String email) throws UsernameNotFoundException {
         return repository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " does not exist in database."));
+    }
+
+    @Transactional
+    public void removeByUsername(String email) {
+        repository.delete(loadUserByUsername(email));
     }
 }
