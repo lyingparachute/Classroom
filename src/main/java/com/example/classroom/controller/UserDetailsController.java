10481186a981432cab3fe916a6b7a6053692428f
lyingparachute <igor1501@o2.pl>
2023-03-19 19:32:09 +0100

    Add model attributes and validate post method

--- src/main/java/com/example/classroom/controller/UserDetailsController.java
+++ src/main/java/com/example/classroom/controller/UserDetailsController.java
@@ -1,28 +1,57 @@
 package com.example.classroom.controller;
 
+import com.example.classroom.model.UserLogin;
 import com.example.classroom.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller("/dashboard/user")
+import javax.validation.Valid;
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/dashboard/profile")
 @RequiredArgsConstructor
 public class UserDetailsController {
 
     private final UserService service;
 
+    public static final String USER_EDIT_FORM = "user/user-edit";
+
     @GetMapping
-    public String getUserDetailsPage() {
+    public String getUserDetailsPage(Model model,
+                                     Principal principal) {
+        addAttributeUserByUsername(model, principal);
         return "user/user-view";
     }
+
 
     @GetMapping("/edit")
-    public String getEdituserDetailsPage() {
-        return "user/user-view";
+    public String getEdituserDetailsPage(Model model,
+                                         Principal principal) {
+        addAttributeUserByUsername(model, principal);
+        return USER_EDIT_FORM;
     }
 
-    @GetMapping("/update")
-    public String updateUserDetails() {
-        return "user/user-view";
+    @PostMapping("/update")
+    public String updateUserDetails(@Valid @ModelAttribute UserLogin user,
+                                    BindingResult result,
+                                    RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            return USER_EDIT_FORM;
+        }
+        UserLogin updated = service.update(user);
+        redirectAttributes.addFlashAttribute("editSuccess", updated);
+        return "redirect:/dashboard/profile";
+    }
+
+    private void addAttributeUserByUsername(Model model, Principal principal) {
+        model.addAttribute("user", service.loadUserByUsername(principal.getName()));
     }
 }
