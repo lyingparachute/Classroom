251289440cf1391568895eb036cbbc212192bcdc
lyingparachute <igor1501@o2.pl>
2023-03-24 21:40:58 +0100

    Add validation to register request

--- src/main/java/com/example/classroom/auth/RegisterRequest.java
+++ src/main/java/com/example/classroom/auth/RegisterRequest.java
@@ -1,19 +1,35 @@
 package com.example.classroom.auth;
 
+import com.example.classroom.auth.validation.ValidPassword;
 import com.example.classroom.enums.RoleEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @Length(min = 2, max = 30, message = "{message.firstName.length}")
     private String firstName;
+
+    @Length(min = 2, max = 30, message = "{message.lastName.length}")
     private String lastName;
+
+    @NotBlank(message = "{message.email.empty}")
+    @Email(message = "{message.email.valid}")
     private String email;
+
+    @ValidPassword
     private String password;
+
+    @NotNull(message = "{role.not.null}")
     private RoleEnum role;
 }
