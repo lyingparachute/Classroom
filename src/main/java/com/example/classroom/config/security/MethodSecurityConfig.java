d46c3e46edad88cd7863701b36ce5a8028754aeb
lyingparachute <igor1501@o2.pl>
2023-03-29 18:41:03 +0200

    Update method security config deleting deprecated annotation

--- src/main/java/com/example/classroom/config/security/MethodSecurityConfig.java
+++ src/main/java/com/example/classroom/config/security/MethodSecurityConfig.java
@@ -2,13 +2,11 @@ package com.example.classroom.config.security;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true,
+@EnableMethodSecurity(
         securedEnabled = true,
         jsr250Enabled = true)
-public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+public class MethodSecurityConfig {
 }
