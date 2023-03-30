dc922a273e01b48e40ccfbce518618a094e5c99b
lyingparachute <igor1501@o2.pl>
2023-03-30 17:58:07 +0200

    Resolve bug on HomeController

--- src/main/java/com/example/classroom/controller/HomeController.java
+++ src/main/java/com/example/classroom/controller/HomeController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping
-    public String getHome(){
+
+    @GetMapping("/")
+    public String getHome() {
         return "home";
     }
 }
