5a8f617c34ab99bd764c1b683d9b0b1d2932c42c
lyingparachute <igor1501@o2.pl>
2023-03-31 09:16:03 +0200

    Add test cases for endpoint null, empty and without edit word.

--- src/test/java/com/example/classroom/breadcrumb/BreadcrumbServiceTest.java
+++ src/test/java/com/example/classroom/breadcrumb/BreadcrumbServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.classroom.breadcrumb;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,31 +9,85 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.assertj.core.groups.Tuple.tuple;
 
 @ExtendWith(MockitoExtension.class)
 class BreadcrumbServiceTest {
 
     @InjectMocks
-    private BreadcrumbService breadcrumbService;
+    private BreadcrumbService service;
 
-    @Test
-    void getBreadcrumbs() {
-        // Given
-        String endpoint = "dashboard/students/edit/12";
+    @Nested
+    class getBreadcrumbs {
 
-        // When
-        List<Breadcrumb> actualBreadcrumbs = breadcrumbService.getBreadcrumbs(endpoint);
+        @Test
+        void returnsListOfBreadcrumbs_givenEndpointWithoutEdit() {
+            // Given
+            String endpoint = "dashboard/students/12/subjects";
 
-        // Then
-        assertThat(actualBreadcrumbs).extracting(
-                Breadcrumb::getLabel,
-                Breadcrumb::getUrl
-        ).contains(
-                tuple("Classroom", "/"),
-                tuple("Dashboard", "dashboard"),
-                tuple("Students", "dashboard/students"),
-                tuple("Edit  /  12", "dashboard/students/edit/12")
-        );
+            // When
+            List<Breadcrumb> actualBreadcrumbs = service.getBreadcrumbs(endpoint);
+
+            // Then
+            assertThat(actualBreadcrumbs).extracting(
+                    Breadcrumb::getLabel,
+                    Breadcrumb::getUrl
+            ).contains(
+                    tuple("Classroom", "/"),
+                    tuple("Dashboard", "dashboard"),
+                    tuple("Students", "dashboard/students"),
+                    tuple("12", "dashboard/students/12"),
+                    tuple("Subjects", "dashboard/students/12/subjects")
+            );
+        }
+
+        @Test
+        void returnsListOfBreadcrumbs_givenEndpointWithEdit() {
+            // Given
+            String endpoint = "dashboard/students/edit/12";
+
+            // When
+            List<Breadcrumb> actualBreadcrumbs = service.getBreadcrumbs(endpoint);
+
+            // Then
+            assertThat(actualBreadcrumbs).extracting(
+                    Breadcrumb::getLabel,
+                    Breadcrumb::getUrl
+            ).contains(
+                    tuple("Classroom", "/"),
+                    tuple("Dashboard", "dashboard"),
+                    tuple("Students", "dashboard/students"),
+                    tuple("Edit  /  12", "dashboard/students/edit/12")
+            );
+        }
+
+        @Test
+        void throwsIllegalArgumentException_givenNull() {
+            // Given
+            String endpoint = null;
+
+            // When
+            Throwable thrown = catchThrowable(() -> service.getBreadcrumbs(endpoint));
+
+            //Then
+            assertThat(thrown)
+                    .isExactlyInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("Invalid endpoint!");
+        }
+
+        @Test
+        void throwsIllegalArgumentException_givenEmptyEndpoint() {
+            // Given
+            String endpoint = "";
+
+            // When
+            Throwable thrown = catchThrowable(() -> service.getBreadcrumbs(endpoint));
+
+            //Then
+            assertThat(thrown)
+                    .isExactlyInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("Invalid endpoint!");
+        }
     }
 }
