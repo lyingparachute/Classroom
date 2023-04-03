78dacfef87f18dcc3067ed5d28d471079cef110d
lyingparachute <igor1501@o2.pl>
2023-04-03 13:53:44 +0200

    Deleted roles and privileges

--- src/main/java/com/example/classroom/user/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.classroom.user;
-
-import com.example.classroom.user.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
