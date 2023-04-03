ea85da055d6973b4df1426741163d293a027996c
lyingparachute <igor1501@o2.pl>
2023-04-03 13:22:55 +0200

    rearrange packages in tests

---
+++ src/integration-test/java/com/example/classroom/subject/SubjectControllerIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.example.classroom.subject;
+
+import com.example.classroom.repository.util.IntegrationTestsInitData;
+import com.example.classroom.teacher.TeacherRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ActiveProfiles("integration")
+@SpringBootTest
+class SubjectControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Autowired
+    private IntegrationTestsInitData integrationTestsInitData;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        integrationTestsInitData.cleanUp();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+}
