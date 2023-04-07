1c4d08d143e1c1540806c7ea3492ef2f83ee6ae8
lyingparachute <igor1501@o2.pl>
2023-04-07 16:49:44 +0200

    Add new tests for signUp  post method

--- src/integration-test/java/com/example/classroom/auth/controller/AuthenticationControllerIntegrationTest.java
+++ src/integration-test/java/com/example/classroom/auth/controller/AuthenticationControllerIntegrationTest.java
@@ -1,6 +1,14 @@
 package com.example.classroom.auth.controller;
 
+import com.example.classroom.auth.model.RegisterRequest;
+import com.example.classroom.student.Student;
+import com.example.classroom.student.StudentRepository;
+import com.example.classroom.teacher.Teacher;
+import com.example.classroom.teacher.TeacherRepository;
 import com.example.classroom.test.util.IntegrationTestsInitData;
+import com.example.classroom.user.User;
+import com.example.classroom.user.UserRepository;
+import com.example.classroom.user.UserRole;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,7 +20,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -22,6 +34,15 @@ class AuthenticationControllerIntegrationTest {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    TeacherRepository teacherRepository;
 
     @Autowired
     private IntegrationTestsInitData initData;
@@ -58,8 +79,158 @@ class AuthenticationControllerIntegrationTest {
         }
 
         @Test
-        void createsNewUserAccount_givenValidRegisterRequest() {
+        void createsNewUserAccount_withAssociatedStudent_givenValidRegisterRequest_withStudentRole() throws Exception {
+            // Given
+            RegisterRequest request = initData.createRegisterRequest();
+            request.setRole(UserRole.ROLE_STUDENT);
 
+            // When
+            mockMvc.perform(post("/sign-up")
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                            .content("firstName=" + request.getFirstName() +
+                                    "&lastName=" + request.getLastName() +
+                                    "&email=" + request.getEmail() +
+                                    "&password=" + request.getPassword() +
+                                    "&role=" + request.getRole()
+                            )
+                    )
+                    .andDo(print())
+                    .andExpect(status().is3xxRedirection());
+
+            // Then
+            Optional<User> userByEmail = userRepository.findByEmail(request.getEmail());
+            Optional<Student> studentByEmail = studentRepository.findByEmail(request.getEmail());
+            Optional<Teacher> teacherByEmail = teacherRepository.findByEmail(request.getEmail());
+
+            assertThat(userByEmail).isPresent();
+            assertThat(studentByEmail).isPresent();
+            assertThat(teacherByEmail).isNotPresent();
+
+            User user = userByEmail.get();
+            Student student = studentByEmail.get();
+
+            assertThat(user.getFirstName()).isEqualTo(student.getFirstName()).isEqualTo(request.getFirstName());
+            assertThat(user.getLastName()).isEqualTo(student.getLastName()).isEqualTo(request.getLastName());
+            assertThat(user.getEmail()).isEqualTo(student.getEmail()).isEqualTo(request.getEmail());
+            assertThat(user.getRole()).isEqualTo(request.getRole());
+            assertThat(user.getStudent()).isNotNull().isEqualTo(student);
+            assertThat(user.getTeacher()).isNull();
+        }
+
+        @Test
+        void createsNewUserAccount_withAssociatedTeacher_givenValidRegisterRequest_withTeacherRole() throws Exception {
+            // Given
+            RegisterRequest request = initData.createRegisterRequest();
+            request.setRole(UserRole.ROLE_TEACHER);
+
+            // When
+            mockMvc.perform(post("/sign-up")
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                            .content("firstName=" + request.getFirstName() +
+                                    "&lastName=" + request.getLastName() +
+                                    "&email=" + request.getEmail() +
+                                    "&password=" + request.getPassword() +
+                                    "&role=" + request.getRole()
+                            )
+                    )
+                    .andDo(print())
+                    .andExpect(status().is3xxRedirection());
+
+            // Then
+            Optional<User> userByEmail = userRepository.findByEmail(request.getEmail());
+            Optional<Student> studentByEmail = studentRepository.findByEmail(request.getEmail());
+            Optional<Teacher> teacherByEmail = teacherRepository.findByEmail(request.getEmail());
+
+            assertThat(userByEmail).isPresent();
+            assertThat(studentByEmail).isNotPresent();
+            assertThat(teacherByEmail).isPresent();
+
+            User user = userByEmail.get();
+            Teacher teacher = teacherByEmail.get();
+
+            assertThat(user.getFirstName()).isEqualTo(teacher.getFirstName()).isEqualTo(request.getFirstName());
+            assertThat(user.getLastName()).isEqualTo(teacher.getLastName()).isEqualTo(request.getLastName());
+            assertThat(user.getEmail()).isEqualTo(teacher.getEmail()).isEqualTo(request.getEmail());
+            assertThat(user.getRole()).isEqualTo(request.getRole());
+            assertThat(user.getStudent()).isNull();
+            assertThat(user.getTeacher()).isNotNull().isEqualTo(teacher);
+        }
+
+        @Test
+        void createsNewUserAccount_withAssociatedTeacher_givenValidRegisterRequest_withDeanRole() throws Exception {
+            // Given
+            RegisterRequest request = initData.createRegisterRequest();
+            request.setRole(UserRole.ROLE_DEAN);
+
+            // When
+            mockMvc.perform(post("/sign-up")
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                            .content("firstName=" + request.getFirstName() +
+                                    "&lastName=" + request.getLastName() +
+                                    "&email=" + request.getEmail() +
+                                    "&password=" + request.getPassword() +
+                                    "&role=" + request.getRole()
+                            )
+                    )
+                    .andDo(print())
+                    .andExpect(status().is3xxRedirection());
+
+            // Then
+            Optional<User> userByEmail = userRepository.findByEmail(request.getEmail());
+            Optional<Student> studentByEmail = studentRepository.findByEmail(request.getEmail());
+            Optional<Teacher> teacherByEmail = teacherRepository.findByEmail(request.getEmail());
+
+            assertThat(userByEmail).isPresent();
+            assertThat(studentByEmail).isNotPresent();
+            assertThat(teacherByEmail).isPresent();
+
+            User user = userByEmail.get();
+            Teacher teacher = teacherByEmail.get();
+
+            assertThat(user.getFirstName()).isEqualTo(teacher.getFirstName()).isEqualTo(request.getFirstName());
+            assertThat(user.getLastName()).isEqualTo(teacher.getLastName()).isEqualTo(request.getLastName());
+            assertThat(user.getEmail()).isEqualTo(teacher.getEmail()).isEqualTo(request.getEmail());
+            assertThat(user.getRole()).isEqualTo(request.getRole());
+            assertThat(user.getStudent()).isNull();
+            assertThat(user.getTeacher()).isNotNull().isEqualTo(teacher);
+        }
+
+        @Test
+        void createsNewUserAccount_givenValidRegisterRequest_withAdminRole() throws Exception {
+            // Given
+            RegisterRequest request = initData.createRegisterRequest();
+            request.setRole(UserRole.ROLE_ADMIN);
+
+            // When
+            mockMvc.perform(post("/sign-up")
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                            .content("firstName=" + request.getFirstName() +
+                                    "&lastName=" + request.getLastName() +
+                                    "&email=" + request.getEmail() +
+                                    "&password=" + request.getPassword() +
+                                    "&role=" + request.getRole()
+                            )
+                    )
+                    .andDo(print())
+                    .andExpect(status().is3xxRedirection());
+
+            // Then
+            Optional<User> userByEmail = userRepository.findByEmail(request.getEmail());
+            Optional<Student> studentByEmail = studentRepository.findByEmail(request.getEmail());
+            Optional<Teacher> teacherByEmail = teacherRepository.findByEmail(request.getEmail());
+
+            assertThat(userByEmail).isPresent();
+            assertThat(studentByEmail).isNotPresent();
+            assertThat(teacherByEmail).isNotPresent();
+
+            User user = userByEmail.get();
+
+            assertThat(user.getFirstName()).isEqualTo(request.getFirstName());
+            assertThat(user.getLastName()).isEqualTo(request.getLastName());
+            assertThat(user.getEmail()).isEqualTo(request.getEmail());
+            assertThat(user.getRole()).isEqualTo(request.getRole());
+            assertThat(user.getStudent()).isNull();
+            assertThat(user.getTeacher()).isNull();
         }
     }
 
