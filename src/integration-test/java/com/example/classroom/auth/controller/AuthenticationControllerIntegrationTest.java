a8c7754522fec0384c904887ee65319a37edb660
lyingparachute <igor1501@o2.pl>
2023-04-07 10:30:45 +0200

    Add test class for auth endpoints

---
+++ src/integration-test/java/com/example/classroom/auth/controller/AuthenticationControllerIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.example.classroom.auth.controller;
+
+import org.junit.jupiter.api.Test;
+
+class AuthenticationControllerIntegrationTest {
+
+    @Test
+    void signIn() {
+    }
+
+    @Test
+    void getSignUpPage() {
+    }
+
+    @Test
+    void signUp() {
+    }
+
+    @Test
+    void getPasswordResetPage() {
+    }
+
+    @Test
+    void resetPassword() {
+    }
+}
