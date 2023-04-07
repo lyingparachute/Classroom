da1d81d8ffb4afb91812d8a27c9f09dad9d5fabc
lyingparachute <igor1501@o2.pl>
2023-04-07 14:14:11 +0200

    Add test for getting view with exact name

--- src/integration-test/java/com/example/classroom/auth/controller/AuthenticationControllerIntegrationTest.java
+++ src/integration-test/java/com/example/classroom/auth/controller/AuthenticationControllerIntegrationTest.java
@@ -1,26 +1,73 @@
 package com.example.classroom.auth.controller;
 
+import com.example.classroom.test.util.IntegrationTestsInitData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ActiveProfiles("integration")
+@SpringBootTest
 class AuthenticationControllerIntegrationTest {
 
-    @Test
-    void signIn() {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private IntegrationTestsInitData initData;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        initData.cleanUp();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
 
-    @Test
-    void getSignUpPage() {
+    @Nested
+    class SignIn {
+        @Test
+        void getsSignInPageView() throws Exception {
+            mockMvc.perform(get("/sign-in"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
+                    .andExpect(view().name("auth/sign-in"));
+        }
     }
 
-    @Test
-    void signUp() {
+    @Nested
+    class SignUp {
+        @Test
+        void getsSignUpPageView() throws Exception {
+            mockMvc.perform(get("/sign-up"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
+                    .andExpect(view().name("auth/sign-up"));
+        }
+
+        @Test
+        void createsNewUserAccount_givenValidRegisterRequest() {
+
+        }
     }
 
-    @Test
-    void getPasswordResetPage() {
-    }
+    @Nested
+    class ResetPassword {
+        @Test
+        void resetPassword() {
 
-    @Test
-    void resetPassword() {
+        }
     }
 }
