c63fdf3710ee9a9b551356f5a5b4d759c184424e
lyingparachute <igor1501@o2.pl>
2023-04-21 19:06:33 +0200

    Add private constructor

--- src/main/java/com/example/classroom/pageable/PageableService.java
+++ src/main/java/com/example/classroom/pageable/PageableService.java
@@ -3,6 +3,8 @@ package com.example.classroom.pageable;
 import org.springframework.data.domain.Page;
 
 public class PageableService {
+    private PageableService() {
+    }
 
     public static long getFirstItemOnPage(Page<?> page, int pageNum, int pageSize) {
         return Math.min((long) pageSize * (pageNum - 1) + 1, page.getTotalElements());
