5839bd11ade97c377a65782a6c864a6fa2fd057d
lyingparachute <igor1501@o2.pl>
2023-04-26 18:53:36 +0200

    Add get endpoint

--- src/main/java/com/example/classroom/user/password/PasswordController.java
+++ src/main/java/com/example/classroom/user/password/PasswordController.java
@@ -3,6 +3,7 @@ package com.example.classroom.user.password;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class PasswordController {
 
+    @GetMapping("forgot-password")
+    String getResetPasswordPage() {
+
+        return "redirect:/sign-up";
+    }
+
     @PostMapping("reset-password")
-    String getResetPasswordPage(@RequestParam("email") String userEmail,
-                                HttpServletRequest request) {
+    String resetPassword(@RequestParam("email") String userEmail,
+                         HttpServletRequest request) {
 
         return "redirect:/sign-up";
     }
