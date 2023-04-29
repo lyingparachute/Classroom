3e6b37d25a05fa3279b9c3c49bae671ca16649aa
lyingparachute <igor1501@o2.pl>
2023-04-29 16:55:07 +0200

    Change log message

--- src/main/java/com/example/classroom/exception/EmailException.java
+++ src/main/java/com/example/classroom/exception/EmailException.java
@@ -4,7 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class EmailException extends RuntimeException {
-    private static final String LOG_ERROR_EXCEPTION_OCCURRED_MSG = "An exception occurred, which will cause a '{}' response";
+    private static final String LOG_ERROR_EXCEPTION_OCCURRED_MSG =
+            "An exception occurred, which will cause a '{}' response.";
+    private static final String LOG_ERROR_EXCEPTION_OCCURRED_WITH_CAUSE_MSG =
+            "An exception occurred, which will cause a '{}' response with cause '{}'.";
 
     public EmailException(String message) {
         super(message);
@@ -13,6 +16,6 @@ public class EmailException extends RuntimeException {
 
     public EmailException(String message, Throwable cause) {
         super(message, cause);
-        log.warn(LOG_ERROR_EXCEPTION_OCCURRED_MSG, message);
+        log.warn(LOG_ERROR_EXCEPTION_OCCURRED_WITH_CAUSE_MSG, message, cause);
     }
 }
