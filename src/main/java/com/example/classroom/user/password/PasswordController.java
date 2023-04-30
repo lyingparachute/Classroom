ddbb140fce871a54b16f84c3327c3676aed1c574
lyingparachute <igor1501@o2.pl>
2023-04-30 12:19:38 +0200

    Add password/change endpoint

--- src/main/java/com/example/classroom/user/password/PasswordController.java
+++ src/main/java/com/example/classroom/user/password/PasswordController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +16,35 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequiredArgsConstructor
 public class PasswordController {
 
+    private static final String REDIRECT_TO_SIGN_IN_PAGE = "redirect:/sign-in";
     private final PasswordService service;
 
     @PostMapping("/reset")
-    String resetPassword(@Valid @RequestParam("email") String userEmail,
-                         HttpServletRequest request,
-                         RedirectAttributes redirectAttributes) {
-        boolean resetResult = service.resetPassword(request, userEmail);
+    String sendResetPasswordEmail(@Valid @RequestParam("email") String userEmail,
+                                  HttpServletRequest request,
+                                  RedirectAttributes redirectAttributes) {
+        boolean resetResult = service.sendEmailWithResetPasswordInstructions(request, userEmail);
         redirectAttributes.addFlashAttribute("resetPassword", userEmail);
         redirectAttributes.addFlashAttribute("resetPasswordResult", resetResult);
-        return "redirect:/sign-in";
+        return REDIRECT_TO_SIGN_IN_PAGE;
+    }
+
+    @GetMapping("/change")
+    String getPasswordChangePage(@Valid @RequestParam("token") String token,
+                                 Model model,
+                                 HttpServletRequest request,
+                                 RedirectAttributes redirectAttributes) {
+//        model.addAttribute("passwordChangeRequest", new PasswordChangeRequest());
+        return "auth/password-change";
+    }
+
+
+    @PostMapping("/update")
+    String changePassword(@Valid @RequestParam("email") String userEmail,
+                          HttpServletRequest request,
+                          RedirectAttributes redirectAttributes) {
+        boolean resetResult = service.sendEmailWithResetPasswordInstructions(request, userEmail);
+        redirectAttributes.addFlashAttribute("changePasswordSuccess", userEmail);
+        return REDIRECT_TO_SIGN_IN_PAGE;
     }
 }
