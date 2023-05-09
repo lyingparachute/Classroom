0e699cc2f6300d972ec6e4a25e9644b5b4e36147
lyingparachute <igor1501@o2.pl>
2023-05-09 15:33:15 +0200

    Enable access to email verification endpoint for everyone

--- src/main/java/com/example/classroom/config/security/SecurityConfig.java
+++ src/main/java/com/example/classroom/config/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     private static final String SIGN_OUT_API = "/api/sign-out";
     private static final String API_AUTH_ENDPOINTS = "/api/auth/**";
     private static final String PASSWORD_CHANGE_ENDPOINTS = "/password/**";
+    private static final String ACCOUNT_VERIFICATION_ENDPOINT = "/account/verify";
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -37,9 +38,10 @@ public class SecurityConfig {
                 //TODO - enable csrf
                 .csrf().disable()
                 .authorizeHttpRequests(requests -> requests
-                                .requestMatchers(HttpMethod.POST, API_AUTH_ENDPOINTS).permitAll()
-                                .requestMatchers(HOME_PAGE, "/css/**", "/js/**", "/assets/**", "/img/home/**", "/webjars/**",
-                                        SIGN_IN_PAGE, SIGN_IN_API, SIGN_UP_PAGE, PASSWORD_CHANGE_ENDPOINTS).permitAll()
+                        .requestMatchers(HttpMethod.POST, API_AUTH_ENDPOINTS).permitAll()
+                        .requestMatchers(HOME_PAGE, "/css/**", "/js/**", "/assets/**", "/img/home/**", "/webjars/**",
+                                SIGN_IN_PAGE, SIGN_IN_API, SIGN_UP_PAGE, PASSWORD_CHANGE_ENDPOINTS,
+                                ACCOUNT_VERIFICATION_ENDPOINT).permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(login -> login
