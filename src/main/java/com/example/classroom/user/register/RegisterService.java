257193a00eb0300c0d353f60fe87e4dbf022eed2
lyingparachute <igor1501@o2.pl>
2023-05-09 15:36:04 +0200

    Add service methods for creating token and verifying account

--- src/main/java/com/example/classroom/user/register/RegisterService.java
+++ src/main/java/com/example/classroom/user/register/RegisterService.java
@@ -1,9 +1,15 @@
 package com.example.classroom.user.register;
 
+import com.example.classroom.exception.InvalidTokenException;
 import com.example.classroom.mail_sender.MailSenderService;
+import com.example.classroom.user.User;
+import com.example.classroom.user.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 import static com.example.classroom.mail_sender.MailSenderService.getAppUrl;
 import static java.util.Map.entry;
@@ -14,20 +20,63 @@ import static java.util.Map.ofEntries;
 public class RegisterService {
 
     private final MailSenderService mailService;
+    private final VerificationTokenRepository tokenRepository;
+    private final UserRepository userRepository;
 
-    void sendRegistrationConfirmationEmail(HttpServletRequest request, RegisterRequest user) {
+    void sendRegistrationConfirmationEmail(final HttpServletRequest request, final User user) {
+        final String verificationToken = createAndSaveEmailVerificationToken(user);
         mailService.sendEmail(user.getEmail(),
-                "Welcome to Classroom",
+                "Welcome to Classroom!",
                 "mail/account-create-confirmation.html",
                 ofEntries(
                         entry("firstName", user.getFirstName()),
-                        entry("confirmLink", getConfirmationLink(request, user.getEmail())),
+                        entry("confirmLink", getRegistrationConfirmationLink(request,
+                                verificationToken)),
                         entry("websiteLink", getAppUrl(request))
                 )
         );
     }
 
-    private String getConfirmationLink(HttpServletRequest request, String userEmail) {
-        return getAppUrl(request) + "/verify/" + userEmail;
+    void validateVerificationToken(final String token) throws InvalidTokenException {
+        final VerificationToken verificationToken = getVerificationToken(token);
+        if (!verificationToken.isValid())
+            throw new InvalidTokenException("Verification Token expired or revoked: " + token);
+    }
+
+    @Transactional
+    void verifyAccount(final String token) {
+        User user = getUserByVerificationToken(token);
+        user.enableAccount();
+        userRepository.save(user);
+        revokeVerificationToken(token);
+    }
+
+    @Transactional
+    private void revokeVerificationToken(final String token) {
+        final VerificationToken verificationToken = getVerificationToken(token);
+        verificationToken.setRevoked();
+        tokenRepository.save(verificationToken);
+    }
+
+    private User getUserByVerificationToken(final String token) {
+        return getVerificationToken(token).getUser();
+    }
+
+    private String getRegistrationConfirmationLink(final HttpServletRequest request, final String token) {
+        return getAppUrl(request) + "/account/verify?token=" + token;
+    }
+
+
+    private VerificationToken getVerificationToken(String token) {
+        return tokenRepository.findByToken(token)
+                .orElseThrow(() -> new InvalidTokenException("Invalid Verification Token: " + token));
+    }
+
+    private String createAndSaveEmailVerificationToken(final User user) {
+        final VerificationToken myToken = new VerificationToken(
+                user,
+                UUID.randomUUID().toString()
+        );
+        return tokenRepository.save(myToken).getToken();
     }
 }
