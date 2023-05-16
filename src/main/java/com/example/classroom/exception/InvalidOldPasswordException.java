78a509aa8f6813c410d88cd3748e2fca9ed631e5
lyingparachute <igor1501@o2.pl>
2023-05-16 19:33:55 +0200

    Change exception name

--- src/main/java/com/example/classroom/exception/InvalidOldPasswordException.java
+++ src/main/java/com/example/classroom/exception/InvalidOldPasswordException.java
@@ -3,19 +3,19 @@ package com.example.classroom.exception;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public final class InvalidOldPasswordExistException extends RuntimeException {
+public final class InvalidOldPasswordException extends RuntimeException {
 
     private static final String LOG_ERROR_EXCEPTION_OCCURRED_MSG =
             "An exception occurred, which will cause a '{}' response.";
     private static final String LOG_ERROR_EXCEPTION_OCCURRED_WITH_CAUSE_MSG =
             "An exception occurred, which will cause a '{}' response with cause '{}'.";
 
-    public InvalidOldPasswordExistException(String message) {
+    public InvalidOldPasswordException(String message) {
         super(message);
         log.warn(LOG_ERROR_EXCEPTION_OCCURRED_MSG, message);
     }
 
-    public InvalidOldPasswordExistException(String message, Throwable cause) {
+    public InvalidOldPasswordException(String message, Throwable cause) {
         super(message, cause);
         log.warn(LOG_ERROR_EXCEPTION_OCCURRED_WITH_CAUSE_MSG, message, cause);
     }
