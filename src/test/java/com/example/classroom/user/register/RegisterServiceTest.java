63cd6508cb2dc06b6418ed97a07932b8a65c21ff
lyingparachute <igor1501@o2.pl>
2023-05-22 14:52:51 +0200

    Create tests for sendAccountVerificationEmail method

--- src/test/java/com/example/classroom/user/register/RegisterServiceTest.java
+++ src/test/java/com/example/classroom/user/register/RegisterServiceTest.java
@@ -1,13 +1,28 @@
 package com.example.classroom.user.register;
 
+import com.example.classroom.exception.AccountAlreadyVerifiedException;
 import com.example.classroom.mail_sender.MailSenderService;
+import com.example.classroom.test.util.UnitTestsInitData;
+import com.example.classroom.user.User;
 import com.example.classroom.user.UserRepository;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.Map;
+
+import static com.example.classroom.mail_sender.MailSenderService.getAppUrl;
+import static java.util.Map.entry;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RegisterServiceTest {
@@ -20,18 +35,78 @@ class RegisterServiceTest {
     VerificationTokenRepository tokenRepository;
     @Mock
     UserRepository userRepository;
+    @Spy
+    MockHttpServletRequest servletRequest;
+    @Spy
+    UnitTestsInitData initData;
 
     @Nested
     class SendAccountVerificationEmail {
         @Test
-        void sendAccountVerificationEmail() {
+        void sendsEmail_givenDisabledUser() {
+            // Given
+            User user = initData.createDisabledUser();
+
+            servletRequest.setServerName("localhost");
+            servletRequest.setServerPort(8080);
+            String emailSubject = "Welcome to Classroom! Verify your email address.";
+            String fileLocation = "mail/account-create-confirmation.html";
+            String token = "verification-token";
+            VerificationToken verificationToken = new VerificationToken(user, token);
+            String confirmLink = "http://localhost:8080/account/verify?token=" + token;
+
+            // When
+            when(tokenRepository.save(any(VerificationToken.class))).thenReturn(verificationToken);
+
+            service.sendAccountVerificationEmail(servletRequest, user);
+
+            // Then
+            verify(tokenRepository).save(any(VerificationToken.class));
+            verify(mailService).sendEmail(
+                    user.getEmail(),
+                    emailSubject,
+                    fileLocation,
+                    Map.ofEntries(entry("firstName", user.getFirstName()),
+                            entry("confirmLink", confirmLink),
+                            entry("websiteLink", getAppUrl(servletRequest)))
+            );
+        }
+
+        @Test
+        void throwsAccountAlreadyVerifiedException_givenEnabledUser() {
+            // Given
+            User user = initData.createUser();
+            servletRequest.setServerName("localhost");
+            servletRequest.setServerPort(8080);
+            String token = "verification-token";
+
+            // When
+            Throwable thrown = catchThrowable(() -> service.sendAccountVerificationEmail(servletRequest, user));
+
+            // Then
+            assertThat(thrown).isExactlyInstanceOf(AccountAlreadyVerifiedException.class)
+                    .hasMessage("Account with email '" + user.getEmail() + "' is already verified.");
         }
     }
 
     @Nested
     class ValidateVerificationToken {
         @Test
-        void validateVerificationToken() {
+        void doesNothing_whenTokenValid() {
+            // Given
+
+            // When
+
+            // Then
+        }
+
+        @Test
+        void throwsInvalidTokenException_whenTokenInvalid() {
+            // Given
+
+            // When
+
+            // Then
         }
     }
 
@@ -39,6 +114,11 @@ class RegisterServiceTest {
     class VerifyAccount {
         @Test
         void verifyAccount() {
+            // Given
+
+            // When
+
+            // Then
         }
     }
 }
