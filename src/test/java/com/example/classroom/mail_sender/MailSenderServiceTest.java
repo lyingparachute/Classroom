d047d9deb5ea260b96c7ff1b2da546152498f1db
lyingparachute <igor1501@o2.pl>
2023-05-29 18:17:20 +0200

    Add testing of exception throwing

--- src/test/java/com/example/classroom/mail_sender/MailSenderServiceTest.java
+++ src/test/java/com/example/classroom/mail_sender/MailSenderServiceTest.java
@@ -1,58 +1,95 @@
 package com.example.classroom.mail_sender;
 
+import com.example.classroom.exception.EmailException;
+import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.ITemplateEngine;
 
 import java.util.Map;
 
+import static com.example.classroom.mail_sender.MailSenderService.getAppUrl;
+import static com.example.classroom.mail_sender.MailSenderService.getSignInLink;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@TestPropertySource(properties = "spring.mail.username=sender@example.com")
 class MailSenderServiceTest {
 
     @InjectMocks
     MailSenderService service;
     @Mock
     JavaMailSender javaMailSender;
+    @Mock
+    MimeMessage mimeMessage;
     @Spy
-    SpringTemplateEngine thymeleafTemplateEngine;
-
+    ITemplateEngine thymeleafTemplateEngine;
     @Spy
     MockHttpServletRequest servletRequest;
+//    @Value("${spring.mail.username}")
+//    private String sender;
 
     @Nested
     class SendEmail {
         @Test
         void createsAndSendsEmail_givenParameters() {
             //Given
-            String userEmail = "user@email.com";
-            String subject = "Email Subject";
+            String userEmail = "test@example.com";
+            String subject = "Test Subject";
             String templateLocation = "/location/template.html";
             Map<String, Object> templateModel = Map.ofEntries(
-                    Map.entry("email", userEmail)
+                    Map.entry("key", "value")
             );
-            String emailBody = "Email body";
-            ArgumentCaptor<SimpleMailMessage> emailCaptor = ArgumentCaptor.forClass(SimpleMailMessage.class);
+            String htmlBody = "Test HTML Body";
+//            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+//            given(thymeleafTemplateEngine.process(anyString(), any())).willReturn(htmlBody);
+
             // When
-//            when(thymeleafTemplateEngine.process(anyString(), any())).thenReturn(emailBody);
-//            when(javaMailSender.createMimeMessage()).thenReturn(MimeMessage.)
+            when(thymeleafTemplateEngine.process(anyString(), any())).thenReturn(htmlBody);
+            when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+//            doReturn("s").when(thymeleafTemplateEngine).process(templateLocation, new Context());
             service.sendEmail(userEmail, subject, templateLocation, templateModel);
 
             // Then
+            then(javaMailSender).should().send(mimeMessage);
+        }
 
-            verify(javaMailSender).send(emailCaptor.capture());
+        @Test
+        void throwsEmailException_whenMessagingExceptionOccurs() {
+            //Given
+            String userEmail = "test@example.com";
+            String subject = "Test Subject";
+            String templateLocation = "/location/template.html";
+            Map<String, Object> templateModel = Map.ofEntries(
+                    Map.entry("key", "value")
+            );
+            String htmlBody = "Test HTML Body";
 
+            // When
+            when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+            when(thymeleafTemplateEngine.process(anyString(), any())).thenReturn(htmlBody);
+            doThrow(new RuntimeException("Failed to send email.")).when(javaMailSender).send(mimeMessage);
+
+            // Then
+            assertThatThrownBy(() -> {
+                service.sendEmail(userEmail, subject, templateLocation, templateModel);
+            })
+                    .isExactlyInstanceOf(EmailException.class)
+                    .hasMessageContaining("Failed to send email.");
         }
     }
 
@@ -64,11 +101,10 @@ class MailSenderServiceTest {
             String expected = "http://localhost:80";
 
             // When
-            String appUrl = MailSenderService.getAppUrl(servletRequest);
+            String appUrl = getAppUrl(servletRequest);
 
             // Then
             assertThat(appUrl).isNotNull().isNotEmpty().isEqualTo(expected);
-
         }
     }
 
@@ -81,7 +117,7 @@ class MailSenderServiceTest {
             String expected = "http://localhost:80/sign-in";
 
             // When
-            String signInLink = MailSenderService.getSignInLink(appUrl);
+            String signInLink = getSignInLink(appUrl);
 
             // Then
             assertThat(signInLink).isNotNull().isNotEmpty().isEqualTo(expected);
