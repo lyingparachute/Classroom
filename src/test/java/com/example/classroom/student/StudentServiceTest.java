92d622e285fd213059ffd82f6aef7b564b8f38e6
lyingparachute <igor1501@o2.pl>
2023-06-07 09:23:26 +0200

    Add test for GetAllStudentsFromRequest method when user is teacher

--- src/test/java/com/example/classroom/student/StudentServiceTest.java
+++ src/test/java/com/example/classroom/student/StudentServiceTest.java
@@ -4,6 +4,7 @@ import com.example.classroom.fieldOfStudy.FieldOfStudy;
 import com.example.classroom.pageable.PageableRequest;
 import com.example.classroom.teacher.Teacher;
 import com.example.classroom.test.util.UnitTestsInitData;
+import com.example.classroom.user.User;
 import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -561,6 +562,62 @@ class StudentServiceTest {
                             .as("Check if %s contains %s", "actualStudent3", "teachers")
                             .contains(teacher1, teacher2, teacher3)
             );
+        }
+    }
+
+
+    @Nested
+    class GetAllStudentsFromRequest {
+        @Test
+        void returnsFilteredAndSortedStudentsPage_givenUserWithRoleTeacher() {
+            // Given
+            int pageNo = 1;
+            int pageSize = 2;
+            String sortField = "name";
+            String sortDirection = Sort.Direction.ASC.name();
+            String name = "wer";
+
+            Teacher teacher1 = initData.createTeacherOne(null, List.of(), List.of());
+            Teacher teacher2 = initData.createTeacherTwo(null, List.of(), List.of());
+            Teacher teacher3 = initData.createTeacherThree(null, List.of(), List.of());
+
+            User user = initData.createUserWithTeacherRole(teacher1);
+
+            FieldOfStudy fieldOfStudy1 = initData.createFieldOfStudyOne(null, List.of(), List.of());
+            FieldOfStudy fieldOfStudy2 = initData.createFieldOfStudyTwo(null, List.of(), List.of());
+
+            Student student1 = initData.createStudentOne(fieldOfStudy1, List.of(teacher1, teacher2));
+            Student student2 = initData.createStudentTwo(fieldOfStudy2, List.of(teacher1, teacher3));
+
+            teacher1.addStudent(student1);
+            teacher1.addStudent(student2);
+
+            PageableRequest pageableRequest = PageableRequest.builder()
+                    .name(name)
+                    .pageNumber(pageNo)
+                    .pageSize(pageSize)
+                    .sortDir(sortDirection)
+                    .sortField(sortField)
+                    .build();
+
+            // When
+            Page<StudentDto> result = service.getAllStudentsFromRequest(pageableRequest, user);
+
+            // Then
+            assertThat(result).isNotEmpty().hasSize(1)
+                    .extracting(
+                            StudentDto::getId,
+                            StudentDto::getFirstName,
+                            StudentDto::getLastName,
+                            StudentDto::getAge,
+                            StudentDto::getEmail,
+                            StudentDto::getTeachers,
+                            StudentDto::getFieldOfStudy
+                    )
+                    .containsExactlyInAnyOrder(
+                            Tuple.tuple(student2.getId(), student2.getFirstName(), student2.getLastName(), student2.getAge(),
+                                    student2.getEmail(), student2.getTeachers(), student2.getFieldOfStudy())
+                    );
         }
     }
 }
